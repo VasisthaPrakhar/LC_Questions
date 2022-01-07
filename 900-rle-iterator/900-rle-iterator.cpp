aaa03184e6e8b2d2d50fd69d817e03698cf20cad
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-07 21:20:41 +0530

    Time: 8 ms (15.84%), Space: 8.8 MB (17.58%) - LeetHub

--- 900-rle-iterator/900-rle-iterator.cpp
+++ 900-rle-iterator/900-rle-iterator.cpp
@@ -1,29 +1,26 @@
 class RLEIterator {
 public:
     long long k=0;
-    vector<long long>s;
-    vector<long long>s1;
+    set<pair<long long,long long>>s;
     RLEIterator(vector<int>& e) {
-        s.push_back(e[0]);
-        s1.push_back(e[1]);
+        s.insert({e[0],e[1]});
         long long sum=e[0];
         for(int i=2;i<e.size();i=i+2){
             if(e[i]>0){
                 sum=sum+e[i];
-                s.push_back(sum);
-                s1.push_back(e[i+1]);
+                s.insert({sum,e[i+1]});   
             }
         }
     }
     
     int next(int n) {
         k+=n;
-        auto it = lower_bound(s.begin(),s.end(),k)-s.begin();
-        if(it==s.size()){
+        auto it = s.lower_bound({k,0});
+        if(it==s.end()){
             return -1;
         }
         else{
-            return s1[it];
+            return it->second;
         }
     }
 };
