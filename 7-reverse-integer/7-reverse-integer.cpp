e4bb34d22f70ddecd27c9b78fe353c8d8e1341f7
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-14 03:00:25 +0530

    Time: 4 ms (41.30%), Space: 6 MB (30.79%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int ans=0;
+        int si=0;
+        if(x<0){
+            si=1;
+            x=abs(x);
+        }
+        while(x){
+            ans=ans*10 + x%10;
+            x=x/10;
+        }
+        if(si==1){
+            ans=ans*-1;
+        }
+        if(ans>2147483647 || ans<-2147483648)
+        {
+            return 0;    
+        }        
+        return ans;
+    }
+};
