4e42a5bc30d32bde8139fc28a2ec440ec05859f0
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-16 01:12:33 +0530

    Time: 328 ms (25.93%), Space: 75.3 MB (37.24%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1){
+            return 0;
+        }
+        unordered_map<int,vector<int>>m;
+        for(int i=0;i<n;i++){
+            m[arr[i]].push_back(i);
+        }
+        vector<int>vis(n,0);
+        queue<int>q;
+        q.push(0);
+        vis[0]=1;
+        int ans=0;
+        while(!q.empty()){
+            int s=q.size();
+            while(s--){
+                int node=q.front();
+                q.pop();
+                if(node==n-1){
+                    return ans;
+                }
+                vector<int>&a=m[arr[node]];
+                a.push_back(node-1);
+                a.push_back(node+1);
+                for(auto x:a){
+                    if(x>=0 && x<n && !vis[x]){
+                        vis[x]=1;
+                        q.push(x);
+                    }
+                }
+                a.clear();
+            }
+            ans++;
+        }
+        return 0;
+    }
+};
