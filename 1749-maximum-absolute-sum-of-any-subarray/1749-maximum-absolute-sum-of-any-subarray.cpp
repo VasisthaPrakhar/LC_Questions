6f712b454951dc9add55a34309f4ead6772103ec
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-18 02:37:45 +0530

    Time: 120 ms (13.27%), Space: 41.4 MB (35.17%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -4,13 +4,17 @@ public:
         int n=nums.size();
         int sum=0,ans1=INT_MIN,ans2=INT_MAX;
         for(int i=0;i<n;i++){
-            sum=max(sum+nums[i],nums[i]);
+            sum=sum+nums[i];
             ans1=max(ans1,sum);
+            if(sum<0){sum=0;}
         }
         sum=0;
         for(int i=0;i<n;i++){
-            sum=min(sum+nums[i],nums[i]);
+            sum=sum+nums[i];
             ans2=min(ans2,sum);
+            if(sum>0){
+                sum=0;
+            }
         }
         return max(abs(ans1),abs(ans2));
     }
