1f2d0bdc3cbbdcbacc19c7f5d011c02f2a5ef430
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-21 02:56:54 +0530

    Time: 12 ms (92.56%), Space: 26.6 MB (15.45%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,12 +11,12 @@
  */
 class Solution {
 public:
-    TreeNode* fun(vector<int>& pr, vector<int>& in,int s1,int e1,int s2,int e2,unordered_map<int,int>&m){
+    TreeNode* fun(vector<int>& pr, vector<int>& in,int s1,int e1,int s2,unordered_map<int,int>&m){
         if(e1-s1<0){
             return nullptr;
         }
         int k=m[pr[s2]];
-        TreeNode* t=new TreeNode(pr[s2],fun(pr,in,s1,k-1,s2+1,s2+k-1-s1,m),fun(pr,in,k+1,e1,s2+k-s1+1,e2,m));
+        TreeNode* t=new TreeNode(pr[s2],fun(pr,in,s1,k-1,s2+1,m),fun(pr,in,k+1,e1,s2+k-s1+1,m));
         return t;
     }
     TreeNode* buildTree(vector<int>& pr, vector<int>& in) {
@@ -25,7 +25,7 @@ public:
         for(int i=0;i<n;i++){
             m[in[i]]=i;
         }
-        return fun(pr,in,0,n-1,0,n-1,m);
+        return fun(pr,in,0,n-1,0,m);
     }
 };
 static auto fast_io = []
