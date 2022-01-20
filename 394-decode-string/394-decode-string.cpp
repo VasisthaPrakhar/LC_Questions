20ebe36c51179e17e81bc9c0cf5b50988fb4a1f5
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-21 02:13:26 +0530

    Time: 4 ms (16.20%), Space: 11.2 MB (7.23%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int n=s.length();
+        int j=0;
+        stack<char>s1;
+        string ans="";
+        while(j<n){
+            if(s[j]!=']'){
+                s1.push(s[j]);
+            }else{
+                string temp="";
+                while(!s1.empty() && s1.top()!='['){
+                    temp=s1.top()+temp;
+                    s1.pop();
+                }
+                s1.pop();
+                string temp1="";
+                while(!s1.empty() && isdigit(s1.top())){
+                    temp1=s1.top()+temp1;
+                    s1.pop();
+                }
+                int k=stoi(temp1);
+                while(k--){
+                    for(auto x:temp){
+                        s1.push(x);
+                    }
+                }
+            }
+            j++;
+        }
+        while(!s1.empty()){
+            ans=s1.top()+ans;
+            s1.pop();
+        }
+        return ans;
+    }
+};
