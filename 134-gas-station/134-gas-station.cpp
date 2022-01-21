75075fa9588dd459d33ba8cb43061c3ea288694d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-21 14:46:15 +0530

    Time: 116 ms (14.73%), Space: 49.9 MB (45.28%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n=gas.size();
-        vector<int>a(n);
-        for(int i=0;i<n;i++){
-            a[i]=gas[i]-cost[i];
-        }
         int sum=0,ans=0,sum1=0;
         for(int i=0;i<n;i++){
-            sum+=a[i];
+            sum+=gas[i]-cost[i];
             if(sum<0){
                 ans=i+1;
                 sum1+=sum;
