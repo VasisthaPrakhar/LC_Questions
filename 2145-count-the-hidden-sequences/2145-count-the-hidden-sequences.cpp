c5543aff19bb1a2f28d6f1763c0346a6ab7d365f
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-24 01:38:35 +0530

    Time: 158 ms (75.00%), Space: 107.4 MB (66.67%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& dif, int lower, int upper) {
-        long long int n=dif.size(),sum=0,mi=INT_MAX,ma=INT_MIN;
+        long long int sum=0;
+        int n=dif.size(),mi=INT_MAX,ma=INT_MIN;
         for(int i=0;i<n;i++){
             sum+=dif[i];
             ma=sum>ma?sum:ma;
             mi=sum<mi?sum:mi;
         }
-        int k;
+        int k,ans;
         if(mi>=0){
             k=lower;
         }else{
             k=lower+abs(mi);
         }
-        int ans=0;
-        while(k>=lower && k<=upper && k+ma>=lower && k+ma<=upper){
-            ans++;
-            k++;
-        }
+        int res=k>k+ma?k:k+ma;
+        ans=(upper-res)<0?0:(upper-res)+1;
         return ans;
     }
 };
+static auto fast_io = []
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+	return 0;
+}();
