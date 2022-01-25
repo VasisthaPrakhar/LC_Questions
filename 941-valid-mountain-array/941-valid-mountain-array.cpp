7787a193c65704cfb352700942671d3d0e1e5401
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-25 23:00:53 +0530

    Time: 16 ms (99.68%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -3,17 +3,17 @@ public:
     bool validMountainArray(vector<int>& arr) {
         int c=0,n=arr.size();
         bool sign=true;
-        if(n<3 || arr[1]-arr[0]<0){
+        if(n<3 || arr[1]<arr[0]){
             return false;
         }
         for(int i=0;i<n-1;i++){
-            if(arr[i+1]-arr[i]==0){
+            if(arr[i+1]==arr[i]){
                 return false;
             }
-            if(sign && arr[i+1]-arr[i]<0){
+            if(sign && arr[i+1]<arr[i]){
                 c++;
                 sign=!sign;
-            }else if(!sign && arr[i+1]-arr[i]>0){
+            }else if(!sign && arr[i+1]>arr[i]){
                 c++;
                 sign=!sign;
             }
@@ -24,3 +24,10 @@ public:
         return false;
     }
 };
+static auto fast_io = []
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+	return 0;
+}();
