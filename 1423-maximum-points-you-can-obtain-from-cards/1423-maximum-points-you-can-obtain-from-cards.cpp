0406a8be8f80a0590fd0be1e937bc7cdd169715c
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-25 22:33:57 +0530

    Time: 82 ms (40.68%), Space: 42.6 MB (26.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxScore(vector<int>& c, int k) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
         int n=c.size();
-        vector<int>p;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=c[i];
-            p.push_back(sum);
+        int p[n];
+        p[0]=c[0];
+        for(int i=1;i<n;i++){
+            p[i]=p[i-1]+c[i];
         }
         if(k==1){
             return max(p[0],p[n-1]-p[n-2]);
@@ -17,7 +18,7 @@ public:
         int ans=p[n-1]-p[(n-1-k)];
         for(int i=1;i<=k;i++){
             int s=p[i-1]+(p[n-1]-p[n-1-k+i]);
-            ans=max(ans,s);
+            ans=ans>s?ans:s;
         }
         return ans;
     }
