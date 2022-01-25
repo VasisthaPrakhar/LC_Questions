40d228f87c7519f295dd5d4a606a3b56a043f16e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-26 02:20:49 +0530

    Time: 6 ms (23.18%), Space: 7.1 MB (65.83%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    void fun(vector<vector<int>>&ans,vector<int>& a,vector<int>& b,int i){
-        if(i>=a.size()){
-            ans.push_back(b);
-            return;
-        }
-        fun(ans,a,b,i+1);
-        b.push_back(a[i]);
-        fun(ans,a,b,i+1);
-        b.pop_back();
-    }
+    // void fun(vector<vector<int>>&ans,vector<int>& a,vector<int>& b,int i){
+    //     if(i>=a.size()){
+    //         ans.push_back(b);
+    //         return;
+    //     }
+    //     fun(ans,a,b,i+1);
+    //     b.push_back(a[i]);
+    //     fun(ans,a,b,i+1);
+    //     b.pop_back();
+    // }
     vector<vector<int>> subsets(vector<int>& a) {
-        vector<int>b;
+        int n=a.size();
+        int lim=pow(2,n);
         vector<vector<int>>ans;
-        fun(ans,a,b,0);
+        for(int i=0;i<lim;i++){
+            vector<int>b;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)){
+                    b.push_back(a[j]);
+                }
+            }
+            ans.push_back(b);
+        }
         return ans;
     }
 };
