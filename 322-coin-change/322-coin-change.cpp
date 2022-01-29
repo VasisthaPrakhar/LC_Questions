d483c72b96c250e4c8dcc52657bfae332f432e53
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-30 03:46:47 +0530

    Time: 81 ms (67.13%), Space: 10.6 MB (82.82%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
-    int dp(vector<int>& co,int am,unordered_map<int,int> &memo,int &n){
-        if(am==0){
-            return 0;
-        }
-        if(!memo.count(am)){
-            int mi=INT_MAX;
-            for(int i=0;i<n;i++){
-                if(am-co[i]>=0){
-                    mi=min((long long)mi,(long long)1+dp(co,am-co[i],memo,n));   
-                }
-            }
-            memo[am]=mi;
-        }
-        return memo[am];
-    }
+    // int dp(vector<int>& co,int am,unordered_map<int,int> &memo,int &n){
+    //     if(am==0){
+    //         return 0;
+    //     }
+    //     if(!memo.count(am)){
+    //         int mi=INT_MAX;
+    //         for(int i=0;i<n;i++){
+    //             if(am-co[i]>=0){
+    //                 mi=min((long long)mi,(long long)1+dp(co,am-co[i],memo,n));   
+    //             }
+    //         }
+    //         memo[am]=mi;
+    //     }
+    //     return memo[am];
+    // }
     int coinChange(vector<int>& co, int am) {
         if(am==0){
             return 0;
         }
         int n=co.size();
-        unordered_map<int,int>memo;
-        for(auto x:co){
-            if(x<=am){
-                memo[x]=1;
+        int dp[n+1][am+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=am;i++){
+            dp[0][i]=INT_MAX-1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=am;j++){
+                if(co[i-1]<=j){
+                    dp[i][j]=min(dp[i-1][j],1+dp[i][j-co[i-1]]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
             }
         }
-        int ans=dp(co,am,memo,n);
-        return ans==INT_MAX?-1:ans;
+        // unordered_map<int,int>memo;
+        // for(auto x:co){
+        //     if(x<=am){
+        //         memo[x]=1;
+        //     }
+        // }
+        // int ans=dp(co,am,memo,n);
+        // return ans==INT_MAX?-1:ans;
+        return dp[n][am]!=INT_MAX-1?dp[n][am]:-1;
     }
 };
