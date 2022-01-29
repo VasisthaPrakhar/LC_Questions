035d1b19fa581ab70ee89c861029a178cc7f31dd
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-29 18:07:14 +0530

    Time: 6 ms (36.60%), Space: 11.2 MB (28.62%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -3,25 +3,26 @@ public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
         int beg=0,end=n-1;
+        if(n==2){
+        }
         while(beg<=end){
             int mid=(beg+end)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            if((nums[mid]>=nums[0] && target>=nums[0]) || (nums[mid]<nums[0] && target<nums[0])){
-                if(target>nums[mid]){
+            if(nums[mid]<target){
+                if(nums[end]<nums[mid] || (nums[mid]<nums[end] && target<=nums[end])){
                     beg=mid+1;
                 }else{
                     end=mid-1;
                 }
             }else{
-                if(nums[mid]<nums[0]){
+                if(nums[beg]>nums[mid] ||(target>=nums[beg] && nums[mid]>nums[beg])){
                     end=mid-1;
                 }else{
                     beg=mid+1;
                 }
             }
-            
         }
         return -1;
     }
