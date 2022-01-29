f8b53d04361f058ed9ab472737ad7187442937f5
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-30 03:23:18 +0530

    Time: 1037 ms (5.01%), Space: 62.8 MB (7.22%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp(vector<int>& co,int am,unordered_map<int,int> &memo,int &n){
+        if(am==0){
+            return 0;
+        }
+        if(!memo.count(am)){
+            int mi=INT_MAX;
+            for(int i=0;i<n;i++){
+                if(am-co[i]>=0){
+                    mi=min((long long)mi,(long long)1+dp(co,am-co[i],memo,n));   
+                }
+            }
+            memo[am]=mi;
+        }
+        return memo[am];
+    }
+    int coinChange(vector<int>& co, int am) {
+        if(am==0){
+            return 0;
+        }
+        int n=co.size();
+        unordered_map<int,int>memo;
+        for(auto x:co){
+            if(x<=am){
+                memo[x]=1;
+            }
+        }
+        int ans=dp(co,am,memo,n);
+        return ans==INT_MAX?-1:ans;
+    }
+};
