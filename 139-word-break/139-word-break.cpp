489a144e85e64b591deb3eb1ecf5f131f77f8a5e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-31 02:52:34 +0530

    Time: 4 ms (90.48%), Space: 7.7 MB (87.59%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wd) {
+        int n=s.length();
+        vector<bool>dp(n+1);
+        dp[0]=true;
+        for(int i=1;i<=n;i++){
+            for(auto x:wd){
+                int l=x.length();
+                if(i>=l && dp[i-l]){
+                    if(s.substr(i-l,l)==x){
+                        dp[i]=true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+};
