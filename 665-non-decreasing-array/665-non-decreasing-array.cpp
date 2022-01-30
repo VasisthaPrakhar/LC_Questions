fa1d9efcf60b0554151031fb133700e048783abb
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-31 04:06:58 +0530

    Time: 43 ms (34.10%), Space: 27 MB (81.75%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         int c=0,prev=0,n=nums.size();
+        if(n < 3)
+        {
+            return true;
+        }
         for(int i=1;i<n;i++){
             if(nums[prev]>nums[i]){
-                if(c++>0){
+                c++;
+                if(c>1){
                     return false;
                 }
                 if (i - 2 >= 0 && nums[i - 2] > nums[i]) continue;
