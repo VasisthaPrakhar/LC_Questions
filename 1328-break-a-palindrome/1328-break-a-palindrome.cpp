07a524ea7233bca6b2a3ddea4beef07b0c865392
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-01-31 01:43:21 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (45.42%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
+    bool check(string s, int i){
+        s[i]='a';
+        string s1=string(s.rbegin(),s.rend());
+        if(s1==s){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     string breakPalindrome(string p) {
-        if(p.length()==0 || p.length()==1){
+        if(p.length()==1){
             return "";
         }
-        string p1=p;
-        int flag=0,cnta=0;
-        for(auto x:p){
-            if(x=='a')
-            cnta++;
+        int i=0,f=1;
+        while(i<p.length()){
+            if(p[i]=='a'){
+                i++;
             }
-        for(auto &x:p1){
-            if(x=='a'){
-                continue;
+            else if(check(p,i)){
+                i++;
             }
             else{
-                x='a';
-                flag=1;
+                f=0;
+                p[i]='a';
                 break;
             }
         }
-        
-        if(p.length()%2!=0 && p[p.length()/2]!='a' && cnta==p.length()-1){
-            p[p.length()-1]='b';
+        if(f==0){
             return p;
         }
-        if(flag==0){
-            p1[p1.length()-1]='b';
+        else{
+            p[p.length()-1] += 1;
+            return p;
         }
-        return p1;
     }
 };
