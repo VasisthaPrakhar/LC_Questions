8af522b163587f462aa81ae58745a91f43a89162
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-02-02 19:52:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.41%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1){
-            return n;
+        if (n <= 1) return n;
+        int one_back = 1;
+        int two_back = 0;
+        
+        for (int i = 2; i <= n; i++) {
+            int temp = one_back;
+            one_back += two_back;
+            two_back = temp;
         }
-       vector<int>dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        
+        return one_back;
     }
 };
