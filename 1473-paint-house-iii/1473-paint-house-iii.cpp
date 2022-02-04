72f97153b96157690e2e1f87917e1a101c29f4f3
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-02-05 02:06:46 +0530

    Time: 176 ms (31.09%), Space: 19.5 MB (14.56%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,25 @@
+typedef long long int ll;
+class Solution {
+public:
+    long long int dp(vector<int>& h, vector<vector<int>>& c, int &m, int &n, int t, int idx, vector<vector<vector<int>>>&memo,int last){
+        if(idx>=m || t<0){
+            return t==0?t:INT_MAX;
+        }
+        if(h[idx]!=0){
+            return dp(h,c,m,n,t-(last!=h[idx]),idx+1,memo,h[idx]);
+        }
+        if(!memo[idx][t][last]){
+            int res=INT_MAX;
+            for(int i=0;i<n;i++){
+                res=min((ll)res,(ll)c[idx][i]+dp(h,c,m,n,t-(last!=(i+1)),idx+1,memo,i+1));
+            }
+            memo[idx][t][last]=res;
+        }
+        return memo[idx][t][last];
+    }
+    int minCost(vector<int>& h, vector<vector<int>>& c, int m, int n, int t) {
+        vector<vector<vector<int>>>memo(m,vector<vector<int>>(t+1,vector<int>(n+1,0)));
+        int ans=dp(h,c,m,n,t,0,memo,0);
+        return ans==INT_MAX?-1:ans;
+    }
+};
