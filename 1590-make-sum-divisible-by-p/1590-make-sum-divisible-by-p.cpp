61b0befb26120995e566d4048a7a339327b02f40
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-02-17 03:25:53 +0530

    Time: 182 ms (86.44%), Space: 70.6 MB (10.22%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
+    Solution() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
     int minSubarray(vector<int>& nums, int p) {
         int n=nums.size();
-        vector<long long int>pre(n);
+        std::vector<long long int>pre(n);
         pre[0]=nums[0];
         for(int i=1;i<n;i++){
             pre[i]=pre[i-1]+nums[i];
@@ -11,14 +15,14 @@ public:
         if(t==0){
             return 0;
         }
-        unordered_map<int,int>m;
+        std::unordered_map<int,int>m;
         m[0]=-1;
         int ans=INT_MAX;
         for(int i=0;i<n;i++){
             int temp=pre[i]%p;
             int k=(temp-t+p)%p;
             if(m.find(k)!=m.end()){
-                ans=min(ans,i-m[k]);
+                ans=std::min(ans,i-m[k]);
             }
             m[temp]=i;
         }
