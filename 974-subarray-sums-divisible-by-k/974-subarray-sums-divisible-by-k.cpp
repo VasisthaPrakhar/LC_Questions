9a662d9ce917c0f91f9163bcc8e8b0cc7c60f970
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-02-17 01:29:32 +0530

    Time: 106 ms (25.23%), Space: 31.8 MB (25.57%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -5,26 +5,26 @@ public:
         if(n==1){
             return nums[0]%k==0?1:0;
         }
-        vector<int>pre(n);
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            pre[i]=sum;
-        }
+        // vector<int>pre(n);
+        // for(int i=0;i<n;i++){
+        //     sum+=nums[i];
+        //     pre[i]=sum;
+        // }
         unordered_map<int,int>m;
         m[0]=1;
         int ans=0;
         for(int i=0;i<n;i++){
-            int temp=(pre[i]%k);
+            sum+=nums[i];
+            int temp=(sum%k);
             if(temp<0){
                 temp+=k;
             }
             if(m.find(temp)!=m.end()){
                 ans+=m[temp];
-                m[temp]++;
-            }else{
-                m[temp]++;
             }
+            m[temp]++;
         }
+        //cout<<-7%5;
         return ans;
     }
 };
