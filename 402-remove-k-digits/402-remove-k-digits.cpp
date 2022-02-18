826f5b0271ce92e3a6f87ca0924b5f500a6ce1d7
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-02-19 02:36:41 +0530

    Time: 3 ms (91.11%), Space: 7 MB (52.42%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -8,27 +8,7 @@ public:
         if(k==0){
             return num;
         }
-        string ans="";
-        // stack<char>t;
-        // t.push(num[0]);
-        // for(int i=1;i<n;i++){
-        //     while(k>0 && !t.empty() && num[i]<t.top()){
-        //         k--;
-        //         t.pop();
-        //     }
-        //     t.push(num[i]);
-        //     if(num[i]=='0' && t.size()==1){
-        //         t.pop();
-        //     }   
-        // }
-        // while(k>0 && !t.empty()){
-        //     k--;
-        //     t.pop();
-        // }
-        // while(!t.empty()){
-        //     ans=t.top()+ans;
-        //     t.pop();
-        // }
+        string ans;
         for(char &x:num){
             while(ans.length() && k>0 && x<ans.back()){
                 ans.pop_back();
