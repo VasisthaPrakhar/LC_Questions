8e85b5531aa0f6900bfe287b3c1c098220763fda
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-02-26 03:00:32 +0530

    Time: 8 ms (97.61%), Space: 10.5 MB (29.44%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -6,14 +6,13 @@ public:
            return a[1]<b[1]; 
         });
         priority_queue<int,vector<int>,greater<int>>pq;
-        vector<int>m(1002,0);
+        unordered_map<int,int>m;
         for(int i=0;i<n;i++){
             while(!pq.empty() && trips[i][1]>=pq.top()){
                 cap+=m[pq.top()];
                 m[pq.top()]=0;
                 pq.pop();
             }
-            cout<<cap<<" ";
             if(cap>=trips[i][0]){
                 cap=cap-trips[i][0];
                 m[trips[i][2]]+=trips[i][0];
