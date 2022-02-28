4fc8ef878acfe5f8ed7c7afbc25958c60fb4fb4b
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-02-28 16:40:09 +0530

    Time: 7 ms (7.02%), Space: 6.9 MB (62.02%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    vector<string> summaryRanges(vector<int>& a) {
+        int n=a.size();
+        vector<string>ans;
+        if(n==0){
+            return ans;
+        }
+        if(n==1){
+            ans.push_back(to_string(a[0]));
+            return ans;
+        }
+        int j=0,i=0;
+        while(j<n){
+            i=j;
+            int k=j;
+                j++;
+            while(j<n && a[k]+1==a[j]){
+                j++,k++;
+            }
+            j--;
+            
+            string st;
+            if(a[j]!=a[i])
+                st=to_string(a[i])+"->"+to_string(a[j]);
+            else
+                st=to_string(a[j]);
+            ans.push_back(st);
+            j++;
+        }
+        return ans;
+    }
+};
