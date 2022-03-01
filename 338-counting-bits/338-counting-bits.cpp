d621c4c3b7096412331e0d9a4a7fd85aa0719d1d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-01 17:14:23 +0530

    Time: 13 ms (20.03%), Space: 8.6 MB (21.58%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        ans.push_back(0);
+        for(int i=1;i<=n;i++){
+            ans.push_back(ans[i&(i-1)]+1);
+        }
+        return ans;
+    }
+};
