849b7f7d33cb95139b563aa93028736506af1180
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-02 22:32:01 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (72.95%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=s.length();
+        int m=t.length();
+        int i=0,j=0;
+        while(j<m){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        return i==n;
+    }
+};
