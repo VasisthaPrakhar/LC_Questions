7555570e623f040893e2cf9bd7520c5d308c420e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-03 01:31:38 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (70.22%) - LeetHub

--- 942-di-string-match/942-di-string-match.cpp
+++ 942-di-string-match/942-di-string-match.cpp
@@ -1,3 +1,10 @@
+static auto fast_io = []
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+	return 0;
+}();
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
