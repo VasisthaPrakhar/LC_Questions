7642f11f371ed3e26cbcceada2c75ab0a605e8ec
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-04 22:12:53 +0530

    Time: 23 ms (37.97%), Space: 17.6 MB (33.42%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     double champagneTower(int p, int qr, int qg) {
-        vector<vector<double>>dp(qr+2,vector<double>(101,0.00));
+        vector<vector<double>>dp(qr+2,vector<double>(101,0.0));
         dp[0][0]=p;
         for(int i=0;i<=qr;i++){
             for(int j=0;j<=i;j++){
-                if(dp[i][j]>=1.00){
+                if(dp[i][j]>=1.0){
                     dp[i+1][j]+=(dp[i][j]-1)/2;
                     dp[i+1][j+1]+=(dp[i][j]-1)/2;
                     dp[i][j]=1;
