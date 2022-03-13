4d72000269e73410447fef84a1c806c25743f1f1
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-13 10:00:05 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.36%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isValid(string st) {
-        if(st.length()%2!=0){
-            return false;
-        }
         stack<char>s;        
         for(auto x:st){
             if(!s.empty() && ((x==')' && s.top()=='(') || (x==']' && s.top()=='[') || (x=='}' && s.top()=='{'))){
