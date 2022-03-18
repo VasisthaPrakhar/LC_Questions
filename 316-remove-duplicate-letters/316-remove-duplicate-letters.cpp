e4698cc9b167dda4c5e2a7fb7bef289189bdf2bc
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-18 22:57:54 +0530

    Time: 3 ms (73.49%), Space: 6.5 MB (92.50%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int>freq(27,0);
+        vector<bool>visited(27,true);
+        for(auto x: s){
+            freq[x-'a']++;
+            visited[x-'a']=false;
+        }
+        int f=0;
+        string ans=" ";
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']--;
+            if(visited[s[i]-'a']==false){
+                if(f==1){
+                    while(s[i]<ans.back() && freq[ans.back()-'a']>0){
+                        visited[ans.back()-'a']=false;
+                        ans.pop_back();
+                    }
+                }
+                ans=ans+s[i];
+                visited[s[i]-'a']=true;
+                f=1;
+                //cout<<ans<<" ";
+            }
+        }
+        return ans.substr(1,ans.length());
+    }
+};
