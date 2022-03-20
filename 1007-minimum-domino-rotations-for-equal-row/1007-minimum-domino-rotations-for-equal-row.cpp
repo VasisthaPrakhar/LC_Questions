e4f074caa53f3d4cacacf5e437fa71705a5a9d6b
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-20 22:46:52 +0530

    Time: 112 ms (92.82%), Space: 111.6 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int solve(int it, vector<int>& t, vector<int>& b){
-        int n=t.size();
+    int solve(int &it, vector<int>& t, vector<int>& b, int &n){
         int ans1=0,ans2=0;
         for(int i=0;i<n;i++){
             if(t[i]!=it && b[i]!=it){
@@ -14,12 +13,14 @@ public:
                 ans2++;
             }
         }
-        return min(ans1,ans2);
+        return ans1<ans2?ans1:ans2;
     }
     int minDominoRotations(vector<int>& t, vector<int>& b) {
+        int n=t.size();
         int ans=INT_MAX;
         for(int i=1;i<=6;i++){
-            ans=min(solve(i,t,b),ans);
+            int temp=solve(i,t,b,n);
+            ans=temp<ans?temp:ans;
         }
         return ans==INT_MAX?-1:ans;
     }
