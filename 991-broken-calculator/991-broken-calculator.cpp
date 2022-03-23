d59529fd1ffa0c1b5b1b09e67639a59ff6d5f03f
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-23 21:17:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.18%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int brokenCalc(int s, int t) {
-        if(s==t){
-            return 0;
-        }
         int ans=0;
         while(s<t){
             ans++;
-            if(t%2){
-                t++;
-            }
+            if(t%2){t++;}
             else
                 t=t/2;
         }
