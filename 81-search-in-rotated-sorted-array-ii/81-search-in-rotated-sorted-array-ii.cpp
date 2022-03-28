32b226be33e6032237abe720e0b176789fed145e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-29 02:49:58 +0530

    Time: 8 ms (60.20%), Space: 14 MB (71.85%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        // int beg=0,end=a.size()-1;
-        // while(beg<=end){
-            // while(beg<end && a[beg+1]==a[beg]){
-            //     beg++;
-            // }
-            // while(end>beg && a[end-1]==a[end]){
-            //     end--;
-            // }
-        //     int mid=(beg+end)/2;
-        //     if(a[mid]==tar){
-        //         return true;
-        //     }
-        //     if(a[mid]<tar){
-        //         if(a[beg]<=a[mid] || a[beg]>tar){
-        //             beg=mid+1;
-        //         }else{
-        //             end=mid-1;
-        //         }
-        //     }else{
-        //         if(a[beg]>a[mid] || a[beg]<=tar){
-        //             end=mid-1;
-        //         }else{
-        //             beg=mid+1;
-        //         }
-        //     }
-        // }
-        // return false;
-         int n=nums.size();
-        int beg=0,end=n-1;
+    bool search(vector<int>& a, int tar) {
+        int beg=0,end=a.size()-1;
         while(beg<=end){
-             while(beg<end && nums[beg+1]==nums[beg]){
+            while(beg<end && a[beg+1]==a[beg]){
                 beg++;
             }
-            while(end>beg && nums[end-1]==nums[end]){
+            while(end>beg && a[end-1]==a[end]){
                 end--;
             }
             int mid=(beg+end)/2;
-            if(nums[mid]==target){
+            if(a[mid]==tar){
                 return true;
             }
-            if(nums[mid]<target){
-                if(nums[end]<nums[mid] || (nums[mid]<nums[end] && target<=nums[end])){
+            if(a[mid]<tar){
+                if(a[beg]<=a[mid] || a[beg]>tar){
                     beg=mid+1;
                 }else{
                     end=mid-1;
                 }
             }else{
-                if(nums[beg]>nums[mid] ||(target>=nums[beg] && nums[mid]>nums[beg])){
+                if(a[beg]>a[mid] || a[beg]<=tar){
                     end=mid-1;
                 }else{
                     beg=mid+1;
                 }
             }
         }
-        return false;
+        return false;        
     }
 };
