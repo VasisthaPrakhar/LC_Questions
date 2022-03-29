c75edb4c758def7e9a044e89ce25a68646180981
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-03-29 22:47:18 +0530

    Time: 67 ms (32.55%), Space: 26.4 MB (11.23%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -4,13 +4,13 @@ public:
         map<vector<int>,vector<string>>m;
         for(auto &x:strs){
             vector<int>a(26,0);
-            for(auto y:x){
+            for(auto &y:x){
                 a[y-'a']++;
             }
             m[a].push_back(x);
         }
         vector<vector<string>>ans;
-        for(auto x:m){
+        for(auto &x:m){
             ans.push_back(x.second);
         }
         return ans;
