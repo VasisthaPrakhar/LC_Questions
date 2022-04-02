a67a4cdab52c58f15a06686651fe23c64330e103
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-03 01:59:13 +0530

    Time: 23 ms (99.95%), Space: 19.7 MB (56.88%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,3 +1,6 @@
+#pragma GCC optimize("fast")
+static auto _ = [] () {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
 class Solution {
 public:
     bool check(string &s, int i, int j){
@@ -11,7 +14,7 @@ public:
     bool validPalindrome(string s) {
         int n=s.length();
         int i=0,j=n-1;
-        while(i<=j){
+        while(i<j){
             if(s[i]!=s[j]){
                 return check(s,i+1,j) || check(s,i,j-1);
             }else{
