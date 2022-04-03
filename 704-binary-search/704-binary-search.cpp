954f91c5247519a0870c2698c8d56bd3b7eec835
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-04 02:35:51 +0530

    Time: 53 ms (45.01%), Space: 27.7 MB (12.65%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,9 +1,20 @@
+#pragma GCC optimize("fast")
+static auto _ = [] () {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        auto it=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        if(it<nums.size() && nums[it]==target){
-            return it;
+        int n=nums.size();
+        int beg=0,end=n-1;
+        while(beg<=end){
+            int mid=(beg+end)>>1;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                beg=mid+1;
+            }else{
+                end=mid-1;
+            }
         }
         return -1;
     }
