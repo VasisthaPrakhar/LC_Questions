2906f483aaa2223513f75c9c6950e9071e3ad33a
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-03 14:15:11 +0530

    Time: 4 ms (73.50%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // int i=0;
-        // do{
-        //     i++;
-        // }while(next_permutation(nums.begin(),nums.end())&& i<1);
         next_permutation(nums.begin(),nums.end());
     }
 };
