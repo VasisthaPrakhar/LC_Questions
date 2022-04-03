f5936a6747c0252cd1abef009b4ef3ce7049aebe
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-04 03:25:29 +0530

    Time: 3 ms (70.91%), Space: 8.6 MB (81.01%) - LeetHub

---
+++ 274-h-index/274-h-index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        int n=c.size();
+        sort(c.begin(),c.end());
+        for(int i=0;i<n;i++){
+            cout<<c[i]<<" ";
+            if(c[i]>=n-i){
+                return n-i;
+            }
+        }
+        return 0;
+    }
+};
