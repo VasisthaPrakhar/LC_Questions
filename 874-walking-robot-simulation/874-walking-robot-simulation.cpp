8de0a61839ee4306d41fe516b0836c07e2dc4944
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-06 03:43:21 +0530

    Time: 203 ms (41.92%), Space: 46 MB (17.03%) - LeetHub

---
+++ 874-walking-robot-simulation/874-walking-robot-simulation.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int robotSim(vector<int>& co, vector<vector<int>>& ob) {
+        int n=co.size();
+        set<vector<int>>s(ob.begin(),ob.end());
+        pair<int,int>p={0,0};
+        map<pair<char,int>,char>m;
+        m[{'N',-1}]='E';
+        m[{'N',-2}]='W';
+        m[{'S',-1}]='W';
+        m[{'S',-2}]='E';
+        m[{'W',-1}]='N';
+        m[{'W',-2}]='S';
+        m[{'E',-1}]='S';
+        m[{'E',-2}]='N';
+        char dir='N';
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(co[i]<0){
+                dir=m[{dir,co[i]}];
+            }else{
+                if(dir=='N'){
+                    for(int j=1;j<=co[i];j++){
+                        p.second++;
+                        if(s.count({p.first,p.second})){
+                            p.second--;
+                            break;
+                        }
+                    }
+                }else if(dir=='S'){
+                    for(int j=1;j<=co[i];j++){
+                        p.second--;
+                        if(s.count({p.first,p.second})){
+                            p.second++;
+                            break;
+                        }
+                    }
+                }else if(dir=='E'){
+                    for(int j=1;j<=co[i];j++){
+                        p.first++;
+                        if(s.count({p.first,p.second})){
+                            p.first--;
+                            break;
+                        }
+                    }
+                }else{
+                    for(int j=1;j<=co[i];j++){
+                        p.first--;
+                        if(s.count({p.first,p.second})){
+                            p.first++;
+                            break;
+                        }
+                    }
+                }
+            }
+            ans=max(ans,(p.first*p.first) + (p.second*p.second));
+        }
+        return ans;
+    }
+};
