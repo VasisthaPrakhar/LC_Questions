cbad2cf0424ddf699b483ad0527af131dd748200
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-08 01:55:14 +0530

    Time: 527 ms (5.08%), Space: 308.5 MB (26.63%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -3,8 +3,8 @@ public:
     int mySqrt(int x) {
         int no=46340;
         vector<long long int> v(no);
-        for(int i=0;i<no;i++){
-            v[i]=(i+1)*(i+1);
+        for(int i=1;i<=no;i++){
+            v[i-1]=i*i;
         }
         int k=0;
         for(int i=v.size()-1;i>=0;i--){
