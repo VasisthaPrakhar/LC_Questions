a7cfa5ec988a8d74855b7a03653a37d67697992d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-08 02:03:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.63%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int no=46340;
-        vector<long long int> v(no);
-        for(int i=1;i<=no;i++){
-            v[i-1]=i*i;
-        }
-        int k=0;
-        for(int i=v.size()-1;i>=0;i--){
-            if(x>=v[i]){
-                k=i+1;
-                break;
+        int beg=0,end=46340,ans;
+        while(beg<=end){
+            int mid=(beg+end)>>1;
+            if(mid*mid<=x){
+                ans=mid;
+                beg=mid+1;
+            }else{
+                end=mid-1;
             }
         }
-        return k;
+        return ans;
     }
 };
