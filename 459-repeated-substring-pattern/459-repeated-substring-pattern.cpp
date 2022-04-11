cb1a993d1c1e0c739640aba16e7d3b986fc63018
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-12 00:47:06 +0530

    Time: 1030 ms (5.06%), Space: 412 MB (9.12%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n=s.length();
+        map<char,int>m;
+        for(auto x:s){
+            m[x]++;
+        }
+        int mi=INT_MAX;
+        for(auto x:m){
+            mi=min(mi,x.second);
+        }
+        if(mi==1){
+            return false;
+        }
+        int l=n/mi;
+        for(;l<=n/2;l++){
+            int k=0,flag=0;
+            string temp=s.substr(k,l);
+            k+=l;
+            while(k<n){
+                string temp1=s.substr(k,l);
+                if(temp!=temp1){
+                    flag=1;
+                    break;
+                }
+                k+=l;
+            }
+            //cout<<flag<<" ";
+            if(flag==0){
+                return true;
+            }
+        }
+        return false;        
+    }
+};
