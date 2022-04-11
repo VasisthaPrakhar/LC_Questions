d4b190cb2f07cd0bb3acc6792b65fcf1374b0a3a
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-12 00:48:57 +0530

    Time: 1275 ms (5.06%), Space: 424.8 MB (6.40%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -2,19 +2,7 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int n=s.length();
-        unordered_map<char,int>m;
-        for(auto x:s){
-            m[x]++;
-        }
-        int mi=INT_MAX;
-        for(auto x:m){
-            mi=min(mi,x.second);
-        }
-        if(mi==1){
-            return false;
-        }
-        int l=n/mi;
-        for(;l<=n/2;l++){
+        for(int l=1;l<=n/2;l++){
             int k=0,flag=0;
             string temp=s.substr(k,l);
             k+=l;
