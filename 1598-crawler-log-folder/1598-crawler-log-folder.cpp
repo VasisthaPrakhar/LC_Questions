bd5beb14d9496fc71fcffe41209f99700160e5da
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-13 01:43:07 +0530

    Time: 13 ms (17.46%), Space: 10.6 MB (40.67%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<int>s;
+        s.push(0);
+        int idx=1;
+        for(auto &x:logs){
+            if(x[0]=='.' && x[1]=='.'){
+                if(s.top()!=0){
+                    s.pop();
+                    idx--;
+                }
+            }else if(x[0]=='.'){
+                
+            }else{
+                s.push(idx++);
+            }
+        }
+        return s.top();
+    }
+};
