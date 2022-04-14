9925e7f749206bc97296532e87e80bc96f3b7f40
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-14 17:28:57 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -4,30 +4,29 @@ using namespace std;
 
 
  // } Driver Code Ends
-
 class Solution
 {
     public:
-    //Function to return max value that can be put in knapsack of capacity W.
-    int fun(int W, int wt[], int val[], int n, vector<vector<int>>&memo){
-         if(n==0 || W==0){
-           return 0;
-       }
-       if(memo[W][n]>=0){
-           return memo[W][n];
-       }
-       if(wt[n-1]>W){
-            memo[W][n]=fun(W,wt,val,n-1,memo);
-       }else{
-            memo[W][n]=max(fun(W,wt,val,n-1,memo),val[n-1]+fun(W-wt[n-1],wt,val,n-1,memo));
-       }
-       return memo[W][n];
+    int memo[1001][1001];
+    int dp(int W, int wt[], int val[], int n){
+        if(W<0 || n==0){
+            return 0;
+        }
+        if(memo[W][n]==-1){
+            if(wt[n-1]<=W){
+                memo[W][n]=max(val[n-1]+dp(W-wt[n-1],wt,val,n-1),dp(W,wt,val,n-1));
+            }else{
+                memo[W][n]=dp(W,wt,val,n-1);
+            }
+        }
+        return memo[W][n];
     }
+    //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-      vector<vector<int>>memo(W+1,vector<int>(n+1,-1));
-      return fun(W,wt,val,n,memo);
+       memset(memo,-1,sizeof(memo));
+       return dp(W,wt,val,n);
     }
 };
 
