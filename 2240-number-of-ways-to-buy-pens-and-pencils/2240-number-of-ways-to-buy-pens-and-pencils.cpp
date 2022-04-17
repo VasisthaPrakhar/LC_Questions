6df7ec4c452e0d905b66c6d99e8f28962a325aef
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-17 15:52:31 +0530

    Time: 16 ms (100.00%), Space: 6 MB (100.00%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -2,19 +2,9 @@ class Solution {
 public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
         long long ans=0;
-        if(cost1>total && cost2>total){
-            return 1;
-        }
-        if(cost1>cost2){
-            for(int i=total;i>=0;i=i-cost1){
-                int k=i/cost2;
-                ans+=k+1;
-            }
-        }else{
-            for(int i=total;i>=0;i=i-cost2){
-                int k=i/cost1;
-                ans+=k+1;
-            }
+        for(int i=total;i>=0;i=i-cost1){
+            int k=i/cost2;
+            ans+=k+1;
         }
         return ans;
     }
