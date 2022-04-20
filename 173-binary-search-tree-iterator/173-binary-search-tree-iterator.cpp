b7e8ce23d2e8b733389b5d7057588120ffdeaae4
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-20 14:44:07 +0530

    Time: 23 ms (96.06%), Space: 24.4 MB (10.53%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -11,28 +11,41 @@
  */
 class BSTIterator {
     private: stack<TreeNode*>s;
+    vector<int>a;
+    int idx=-1,n;
+    void fun(TreeNode* root){
+        if(root==nullptr){
+            return;
+        }
+        fun(root->left);
+        a.push_back(root->val);
+        fun(root->right);
+    }
 public:
     BSTIterator(TreeNode* root) {
-        while(root){
-            s.push(root);
-            root=root->left;
-        }
+        // while(root){
+        //     s.push(root);
+        //     root=root->left;
+        // }
+        fun(root);
+        n=a.size();
     }
     
     int next() {
-        TreeNode *t=s.top();
-        s.pop();
-        int val=t->val;
-        t=t->right;
-        while(t){
-            s.push(t);
-            t=t->left;
-        }
-        return val;
+        // TreeNode *t=s.top();
+        // s.pop();
+        // int val=t->val;
+        // t=t->right;
+        // while(t){
+        //     s.push(t);
+        //     t=t->left;
+        // }
+        // return val;
+        return a[++idx];
     }
     
     bool hasNext() {
-        return !s.empty();
+        return idx<n-1;
     }
 };
 
