4cd9999a2369e93b4b3cc1ded8e995e18b852607
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-04-30 01:33:58 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -11,12 +11,14 @@ class Solution{
 	{
         // Your code goes here
         int dp[n+1][sum+1];
-        for(int i=0;i<=sum;i++){
-            dp[0][i]=0;
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
+        // for(int i=0;i<=sum;i++){
+        //     dp[0][i]=0;
+        // }
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0]=1;
+        // }
+        memset(dp,0,sizeof(dp));
+        dp[0][0]=1;
         for(int i=1;i<=n;i++){
             for(int j=0;j<=sum;j++){
                 if(j>=arr[i-1]){
