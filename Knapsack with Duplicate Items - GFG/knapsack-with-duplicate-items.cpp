b50b85d62c0d9390a96c27286af90e933f45d09b
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-05-02 23:05:44 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,27 +9,36 @@ using namespace std;
 
 class Solution{
 public:
-    int dp[1001][1001]; 
-    int fun(int n, int w, int val[], int wt[]){
-        if(w>=0 && n<0){
-            return 0;
-        }
-        if(w<0){
-            return INT_MIN;
-        }
-        if(dp[n][w]==-1){
-            int a=val[n]+fun(n-1,w-wt[n],val,wt);
-            int b=val[n]+fun(n,w-wt[n],val,wt);
-            int c=fun(n-1,w,val,wt);
-            dp[n][w]=max(a,max(b,c));
-        }
-        return dp[n][w];
-    }
+    // int dp[1001][1001]; 
+    // int fun(int n, int w, int val[], int wt[]){
+    //     if(w>=0 && n<0){
+    //         return 0;
+    //     }
+    //     if(w<0){
+    //         return INT_MIN;
+    //     }
+    //     if(dp[n][w]==-1){
+    //         int a=val[n]+fun(n-1,w-wt[n],val,wt);
+    //         int b=val[n]+fun(n,w-wt[n],val,wt);
+    //         int c=fun(n-1,w,val,wt);
+    //         dp[n][w]=max(a,max(b,c));
+    //     }
+    //     return dp[n][w];
+    // }
     int knapSack(int n, int w, int val[], int wt[])
     {
         // code here
-         memset(dp,-1,sizeof dp);
-        return fun(n-1,w,val,wt);
+         vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+         for(int i=1;i<=n;i++){
+             for(int j=1;j<=w;j++){
+                 if(j>=wt[i-1]){
+                     dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i][j-wt[i-1]]);
+                 }else{
+                     dp[i][j]=dp[i-1][j];
+                 }
+             }
+         }
+         return dp[n][w];
     }
         
 };
