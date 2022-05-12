32cfe05d5b16ac7a460267acb5c68d9dfdc54523
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-05-12 17:44:19 +0530

    Time: 33 ms (85.08%), Space: 7.4 MB (84.48%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n=s.length();
+        string t=s;
+        reverse(s.begin(),s.end());
+        int dp[n+1][n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+};
