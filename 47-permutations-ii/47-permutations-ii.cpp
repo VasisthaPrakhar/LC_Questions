6278361ed223dfa02c3c2705ce5a537e2a70cfca
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-05-12 17:52:02 +0530

    Time: 85 ms (18.96%), Space: 9.7 MB (44.45%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    set<vector<int>>s;
+    void fun(vector<int>& a,int n,int idx,vector<int>&vis,vector<int>&res){
+        if(idx>=n){
+            s.insert(res);
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                vis[i]=1;
+                res.push_back(a[i]);
+                fun(a,n,idx+1,vis,res);
+                vis[i]=0;
+                res.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>res,vis(n,0);
+        fun(nums,n,0,vis,res);
+        vector<vector<int>>ans;
+        for(auto x:s){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
