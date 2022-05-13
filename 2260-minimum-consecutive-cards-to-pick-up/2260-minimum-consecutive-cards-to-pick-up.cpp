e9ce277863d9efa0e898d6ad3f3b22a9f155c53c
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-05-14 02:28:18 +0530

    Time: 348 ms (89.56%), Space: 115 MB (93.10%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -3,7 +3,7 @@ public:
     int minimumCardPickup(vector<int>& c) {
         int n=c.size();
         unordered_map<int,int>m;
-        int i=0,j=0,ans=INT_MAX;
+        int i=0,j=0,ans=1e6+1;
         while(j<n){
             m[c[j]]++;
             while(i<=j && m[c[j]]>1){
@@ -15,6 +15,6 @@ public:
             }
             j++;
         }
-        return ans==INT_MAX?-1:ans;
+        return ans==1e6+1?-1:ans;
     }
 };
