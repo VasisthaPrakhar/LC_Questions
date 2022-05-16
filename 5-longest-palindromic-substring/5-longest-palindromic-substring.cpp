afb5935927461950f0fbab1994c72e5e1fbb2af9
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-05-16 21:46:15 +0530

    Time: 18 ms (90.64%), Space: 7.1 MB (92.41%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.length(),ma=0,left;
+       int n=s.length(),ma=0,left;
         for(int i=0;i<n;i++){
             int r=i;
             while(r<n && s[i]==s[r]){
