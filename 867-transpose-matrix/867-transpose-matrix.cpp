8cfd1228cdd7b8b8d8424fe8a57f077951cbfc95
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-06-03 00:52:34 +0530

    Time: 19 ms (34.70%), Space: 10.6 MB (78.16%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>a(m,vector<int>(n));
+        vector<vector<int>>a(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 a[i][j]=mat[j][i];
