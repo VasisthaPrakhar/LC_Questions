62a21915c555158b9829b186aebdcfea9cdb6405
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-06-13 23:10:48 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (68.05%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int t) {
+        int n=nums.size();
         unsigned long long int dp[t+1];
         memset(dp,0,sizeof(dp));
         dp[0]=1;
         for(int i=1;i<=t;i++){
-            for(auto x:nums){
-                if(i>=x)
-                    dp[i]+=dp[i-x];
+            for(int j=0;j<n;j++){
+                if(i>=nums[j]){
+                    dp[i]+=dp[i-nums[j]];
+                }
             }
         }
         return dp[t];
