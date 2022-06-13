35d073b5e62151d0cab30317f3abd52393ac375d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-06-13 22:45:23 +0530

    Added solution - LeetHub

--- Get Minimum Squares - GFG/get-minimum-squares.cpp
+++ Get Minimum Squares - GFG/get-minimum-squares.cpp
@@ -22,13 +22,13 @@ class Solution{
 	    for(int i=0;i<=n;i++){
 	        dp[0][i]=INT_MAX-1;
 	    }
-	    for(int i=1;i<=n;i++){
-	        if(i%a[0]){
-	            dp[1][i]=INT_MAX-1;
-	        }else{
-	            dp[1][i]=i/a[0];
-	        }
-	    }
+	   // for(int i=1;i<=n;i++){
+	   //     if(i%a[0]){
+	   //         dp[1][i]=INT_MAX-1;
+	   //     }else{
+	   //         dp[1][i]=i/a[0];
+	   //     }
+	   // }
 	    for(int i=1;i<=m;i++){
 	        for(int j=1;j<=n;j++){
 	            if(j>=a[i-1]){
