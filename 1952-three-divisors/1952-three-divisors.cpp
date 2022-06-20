9f24664d4a9d3f118138bb88ba0e22cee444d717
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-06-20 22:40:52 +0530

    Time: 3 ms (41.89%), Space: 5.7 MB (99.38%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        if(n<4){return false;}
+        int cnt=0;
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                cnt++;
+            }
+        }
+        if(cnt==1){
+            return true;
+        }
+        return false;
+    }
+};
