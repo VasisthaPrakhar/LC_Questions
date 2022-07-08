cf9f35311be233de175c160553e056d47a7a4e7d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-07-08 13:12:20 +0530

    Time: 431 ms (14.94%), Space: 45.4 MB (42.99%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(string &s,map<string,vector<string>>&adj,vector<string>&res,set<string>&vis){
+        vis.insert(s);
+        res.push_back(s);
+        for(auto x:adj[s]){
+            if(vis.find(x)==vis.end()){
+                dfs(x,adj,res,vis);
+            }
+        }
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& acc) {
+        vector<vector<string>>ans;
+        map<string,vector<string>>adj;
+        set<string>vis;
+        int n=acc.size();
+        for(int i=0;i<n;i++){
+            for(int j=2;j<acc[i].size();j++){
+                adj[acc[i][j-1]].push_back(acc[i][j]);
+                adj[acc[i][j]].push_back(acc[i][j-1]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(vis.find(acc[i][1])==vis.end()){
+                vector<string>res;
+                res.push_back(acc[i][0]);
+                dfs(acc[i][1],adj,res,vis);
+                sort(res.begin()+1,res.end());
+                ans.push_back(res);
+            }
+        }
+        return ans;
+    }
+};
