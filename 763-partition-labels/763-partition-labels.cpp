025fdafeb3055a78d4a0fc6531e0159a05a36019
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-08-01 01:16:52 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (48.42%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n=s.length();
-        vector<int>freq(26,-1);
+        vector<int>last_pos(26,-1);
         for(int i=0;i<n;i++){
-            freq[s[i]-'a']=i;
+            last_pos[s[i]-'a']=i;
         }
         int left=0,right=-1;
         vector<int>ans;
         for(int i=0;i<n;i++){
-            right=freq[s[i]-'a']>right?freq[s[i]-'a']:right;
+            right=last_pos[s[i]-'a']>right?last_pos[s[i]-'a']:right;
             if(right==i){
-                ans.push_back(i-left+1);
+                ans.push_back(right-left+1);
                 right=-1;
                 left=i+1;
             }
