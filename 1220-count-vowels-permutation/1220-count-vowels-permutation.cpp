1f7cc56441c630a2342fee93fe6ba1621121d36a
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-08-07 15:46:31 +0530

    Time: 1551 ms (5.14%), Space: 118 MB (5.29%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,42 +1,43 @@
-#define mod 1000000007
 class Solution {
+private:
+    int mod=1000000007;
 public:
-    int dp(int idx,int prev,map<int,map<int,int>> &memo,int &n){
+    vector<char>vow={'a','e','i','o','u'};
+    int dp(int idx,int prev,unordered_map<int,map<int,int>>&m,int n){
         if(idx==n){
             return 1;
         }
-        if(!memo.count(idx) || !memo[idx].count(prev)){
-            int ans=0;
+        if(!m.count(idx) || !m[idx].count(prev)){
+            int res=0;
             if(idx==0){
                 for(int i=0;i<5;i++){
-                    ans=(ans+dp(idx+1,i,memo,n)%mod)%mod;
+                    res=(res + dp(idx+1,i,m,n)%mod)%mod;
                 }
             }else{
                 if(prev==0){
-                    ans=(ans+dp(idx+1,1,memo,n)%mod)%mod;
+                    res=(res+dp(idx+1,1,m,n)%mod)%mod;
                 }else if(prev==1){
-                    ans=(ans+dp(idx+1,0,memo,n)%mod)%mod;
-                    ans=(ans+dp(idx+1,2,memo,n)%mod)%mod;
+                    res=(res + dp(idx+1,0,m,n)%mod)%mod;
+                    res=(res + dp(idx+1,2,m,n)%mod)%mod;
                 }else if(prev==2){
                     for(int i=0;i<5;i++){
-                        if(i!=2){
-                            ans=(ans+dp(idx+1,i,memo,n)%mod)%mod;
-                        }
+                        if(i!=2)
+                            res=(res+dp(idx+1,i,m,n)%mod)%mod;
                     }
-                }
-                else if(prev==3){
-                    ans=(ans+dp(idx+1,2,memo,n)%mod)%mod;
-                    ans=(ans+dp(idx+1,4,memo,n)%mod)%mod;
+                }else if(prev==3){
+                    res=(res+dp(idx+1,2,m,n)%mod)%mod;
+                    res=(res+dp(idx+1,4,m,n)%mod)%mod;
                 }else{
-                    ans=(ans+dp(idx+1,0,memo,n)%mod)%mod;
+                    res=(res+dp(idx+1,0,m,n)%mod)%mod;
                 }
             }
-            memo[idx][prev]=ans;
+            //cout<<res;
+            m[idx][prev]=res%mod;
         }
-        return memo[idx][prev];
+        return m[idx][prev];
     }
     int countVowelPermutation(int n) {
-         map<int,map<int,int>>memo;
-        return dp(0,0,memo,n);
+        unordered_map<int,map<int,int>>m;
+        return dp(0,0,m,n);
     }
 };
