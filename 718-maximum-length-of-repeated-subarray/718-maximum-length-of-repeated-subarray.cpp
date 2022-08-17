704803818c2e8c364adf6bbf1432002fe110ae38
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-08-17 17:53:55 +0530

    Time: 270 ms (85.66%), Space: 16.1 MB (68.01%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -4,13 +4,14 @@ public:
         int n=text1.size();
         int m=text2.size();
         int ans=INT_MIN;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(text1[i]==text2[j]){
-                    dp[i+1][j+1]=1+dp[i][j];
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
-                ans=max(ans,dp[i+1][j+1]);
+                ans=max(ans,dp[i][j]);
             }
         }
         return ans;
