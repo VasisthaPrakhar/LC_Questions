26b99e9a8f075ee1acb89ea5f9e6e882c5a914f1
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-08-23 19:34:06 +0530

    Time: 363 ms (45.98%), Space: 9.1 MB (65.53%) - LeetHub

---
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int memo[10001][7][7];
+    int mod=1000000007;
+    int fun(int n,int prev,int prevprev){
+        if(n==0){
+            return 1;
+        }
+        if(memo[n][prev][prevprev]==-1){
+            long long int res=0;
+            for(int i=1;i<=6;i++){
+                if(i!=prev && i!=prevprev && (prev==0 || __gcd(prev,i)==1)){
+                    res=(res%mod + fun(n-1,i,prev)%mod)%mod;
+                }
+            }
+            memo[n][prev][prevprev]=res;
+        }
+        return memo[n][prev][prevprev];
+    }
+    int distinctSequences(int n) {
+        memset(memo,-1,sizeof(memo));
+        return fun(n,0,0);
+    }
+};
