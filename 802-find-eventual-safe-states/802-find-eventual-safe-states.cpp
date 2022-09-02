d1ed3f368f5c92c7aad46dec2c068bde9e6aad6f
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-09-02 13:55:47 +0530

    Time: 446 ms (11.04%), Space: 93.6 MB (6.09%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& g) {
+        int n=g.size();
+        unordered_map<int,unordered_set<int>>adj;
+        vector<int>ind(n,0),ans;
+        for(int i=0;i<n;i++){
+            for(auto x:g[i]){
+                adj[x].insert(i);
+                ind[i]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(ind[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto x:adj[node]){
+                ind[x]--;
+                if(ind[x]==0){
+                    q.push(x);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
