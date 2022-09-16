55f887307d472cb1471e15a0158736590ffd75c8
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-09-16 21:18:44 +0530

    Time: 21 ms (14.61%), Space: 13.3 MB (33.31%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -43,7 +43,6 @@ public:
             }
         }
         ans=max(bfs(grid,qu),ans);
-        //cout<<ans<<" ";
          for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
