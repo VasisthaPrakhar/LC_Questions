2bad146daea90ddc72011ab90c0e087db23a5a83
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-09-25 03:05:51 +0530

    Time: 43 ms (41.24%), Space: 22.9 MB (31.54%) - LeetHub

--- 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int ans=0;
-    void fun(vector<vector<int>>& grid, int num, int col, int &n, int &m){ 
+    int fun(vector<vector<int>>& grid, int num, int col, int &n, int &m){ 
         if(num<=0){
             int cnt=0;
             for(int i=0;i<n;i++){
@@ -16,11 +15,10 @@ public:
                     cnt++;
                 }
             }
-            ans=max(ans,cnt);
-            return;
+            return cnt;
         }
         if(col>=m){
-            return;
+            return 0;
         }
         vector<int>r;
         for(int i=0;i<n;i++){
@@ -29,16 +27,16 @@ public:
                 grid[i][col]=3;
             }
         }
-        fun(grid,num-1,col+1,n,m);
+        int res1=fun(grid,num-1,col+1,n,m);
         for(auto x:r){
             grid[x][col]=1;
         }
-        fun(grid,num,col+1,n,m);
+        int res2=fun(grid,num,col+1,n,m);
+        return max(res1,res2);
     }
     int maximumRows(vector<vector<int>>& grid, int num) {
         int n=grid.size();
         int m=grid[0].size();
-        fun(grid,num,0,n,m);
-        return ans;
+        return fun(grid,num,0,n,m);
     }
 };
