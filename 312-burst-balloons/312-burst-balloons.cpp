ac3dc23b0573ec633950c59af3893db65e88b66d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-09-26 23:56:14 +0530

    Time: 405 ms (86.26%), Space: 8.3 MB (88.34%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
+        int dp[n+2][n+2];
+        memset(dp,0,sizeof(dp));
+        nums.insert(nums.begin(),1);
+        nums.insert(nums.end(),1);
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    continue;
+                }
+                int ma=INT_MIN;
+                for(int idx=i;idx<=j;idx++){
+                    ma=max(ma,nums[i-1]*nums[idx]*nums[j+1] + dp[idx+1][j] + dp[i][idx-1]);
+                }
+                dp[i][j]=ma;
+            }
+        }
+        return dp[1][n];
+    }
+};
