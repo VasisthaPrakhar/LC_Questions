3834ccf1e280659f1b0000f3d8004b6d4e461d4f
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-10-29 22:39:19 +0530

    Added solution - LeetHub

--- Phone directory - GFG/phone-directory.cpp
+++ Phone directory - GFG/phone-directory.cpp
@@ -6,26 +6,22 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
+
 struct Node {
 	Node *links[26];
 	bool flag = false;
-	//checks if the reference trie is present or not
 	bool containKey(char ch) {
 		return (links[ch - 'a'] != NULL);
 	}
-	//creating reference trie
 	void put(char ch, Node *node) {
 		links[ch - 'a'] = node;
 	}
-	//to get the next node for traversal
 	Node *get(char ch) {
 		return links[ch - 'a'];
 	}
-	//setting flag to true at the end of the word
 	void setEnd() {
 		flag = true;
 	}
-	//checking if the word is completed or not
 	bool isEnd() {
 		return flag;
 	}
@@ -35,18 +31,15 @@ private:
 	Node* root;
 public:
 	Trie() {
-		//creating new obejct
 		root = new Node();
 	}
 
 	void insert(string word) {
-		//initializing dummy node pointing to root initially
 		Node *node = root;
 		for (int i = 0; i < word.size(); i++) {
 			if (!node->containKey(word[i])) {
 				node->put(word[i], new Node());
 			}
-			//moves to reference trie
 			node = node->get(word[i]);
 		}
 		node->setEnd();
