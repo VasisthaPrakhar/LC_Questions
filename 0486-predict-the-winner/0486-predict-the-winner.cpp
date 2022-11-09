1792d46a52ea3621c1dd17d08610804596f0a959
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-11-09 23:13:35 +0530

    Time: 4 ms (72.05%), Space: 7.4 MB (61.83%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     typedef long long int ll;
+    ll dp[21][21];
     ll fun(int i,int j,vector<int>&nums,int flag){
         if(i>j){
             return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         ll res=0;
         if(!flag){
@@ -11,14 +15,13 @@ public:
         }else{
             res=min(-nums[i]+fun(i+1,j,nums,0),-nums[j]+fun(i,j-1,nums,0));
         }
-        return res;
+        return dp[i][j]=res;
     }
     bool PredictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        ll t=accumulate(nums.begin(),nums.end(),0LL);
         int i=0,j=n-1,ans=0;
+        memset(dp,-1,sizeof(dp));
         ll res=fun(i,j,nums,ans);
-        cout<<res<<endl;
         return res>=0;
     }
 };
