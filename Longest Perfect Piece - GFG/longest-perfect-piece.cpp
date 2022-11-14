c278c370f2e248965ce0a28bce1fadedf15ee7ae
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-11-15 01:17:27 +0530

    Added solution - LeetHub

---
+++ Longest Perfect Piece - GFG/longest-perfect-piece.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int longestPerfectPiece(int arr[], int N) {
+        // code here
+        int i=0,j=0,ans=0;
+        while(j<N){
+            while(i<j && abs(arr[i]-arr[j])>1){
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        for(int i=0; i<N; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout << ob.longestPerfectPiece(arr,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
