924771ce0487120ca0c0df3221b6cff9a44ce826
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-12-05 01:31:05 +0530

    Time: 185 ms (68.98%), Space: 99.7 MB (5.21%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
         int n=nums.size();
-        vector<double>pre,suf(n,0);
-        double sum1=0,sum2=0;
+        vector<long long int>pre,suf(n,0);
+        long long int sum1=0,sum2=0;
         for(int i=0;i<n;i++){
             sum1+=nums[i];
             pre.push_back(sum1);
             suf[n-i-1]=sum2;
             sum2+=nums[n-i-1];
         }
-        int ans=INT_MAX,z=floor(sum1/(double)n),idx=0;
+        long long int ans=LLONG_MAX,z=sum1/n,idx=0;
         for(int i=0;i<n-1;i++){
-            double a=floor(pre[i]/(double)(i+1));
-            double b=floor(suf[i]/(double)(n-i-1));
+            int a=pre[i]/(i+1);
+            int b=suf[i]/(n-i-1);
             int k=abs(a-b);
             if(ans>k){
                 ans=k;
