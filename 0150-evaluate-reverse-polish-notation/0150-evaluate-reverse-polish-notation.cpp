1df1caa9afa089afa7a4b6448a376938b8f48971
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2022-12-17 23:33:08 +0530

    Time: 23 ms (55.62%), Space: 12.9 MB (9.18%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -9,54 +9,27 @@ public:
         int ans=0;
         for(auto x:tokens){
             if(check(x)){
+                string a=s.top();
+                s.pop();
+                string b=s.top();
+                s.pop();
+                ll aa=stoll(a);
+                ll bb=stoll(b),c;
                 if(x=="+"){
-                    string a=s.top();
-                    s.pop();
-                    string b=s.top();
-                    s.pop();
-                    ll aa=stoll(a);
-                    ll bb=stoll(b);
-                    ll c=aa+bb;
-                    string k=to_string(c);
-                    s.push(k);
+                    c=aa+bb;
                 }else if(x=="-"){
-                    string a=s.top();
-                    s.pop();
-                    string b=s.top();
-                    s.pop();
-                    ll aa=stoll(a);
-                    ll bb=stoll(b);
-                    ll c=bb-aa;
-                    string k=to_string(c);
-                    s.push(k);
+                    c=bb-aa;
                 }else if(x=="*"){
-                    string a=s.top();
-                    s.pop();
-                    string b=s.top();
-                    s.pop();
-                    ll aa=stoll(a);
-                    ll bb=stoll(b);
-                    ll c=aa*bb;
-                    string k=to_string(c);
-                    s.push(k);
+                    c=aa*bb;
                 }else{
-                    string a=s.top();
-                    s.pop();
-                    string b=s.top();
-                    s.pop();
-                    ll aa=stoll(a);
-                    ll bb=stoll(b);
-                    ll c=bb/aa;
-                    //cout<<c<<"&&&"<<endl;
-                    string k=to_string(c);
-                    s.push(k);
+                    c=bb/aa;
                 }
+                string k=to_string(c);
+                s.push(k);
             }else{
                 s.push(x);
             }
-            //cout<<s.top()<<" ";
         }
-        //cout<<endl;
         return stoll(s.top());
     }
 };
