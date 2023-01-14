69c1a3e21d85641bfd53ce232388dccfd58bcd3e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-01-15 00:38:06 +0530

    Time: 6 ms (49.54%), Space: 6.6 MB (60.55%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findpar(int u,vector<int>&par){
+        if(u==par[u])
+            return u;
+        return findpar(par[u],par);
+    }
+    void unionn(int u,int v,vector<int>&par){
+        u=findpar(u,par);
+        v=findpar(v,par);
+        par[max(u,v)]=min(u,v);
+    }
+    string smallestEquivalentString(string s1, string s2, string b) {
+        int n=s1.length();
+        vector<int>par(26,0);
+        for(int i=0;i<26;i++){
+            par[i]=i;
+        }
+        for(int i=0;i<n;i++){
+            unionn(s1[i]-'a',s2[i]-'a',par);
+        }
+        string ans="";
+        for(auto x:b){
+            ans+=findpar(x-'a',par)+'a';
+        }
+        return ans;
+    }
+};
