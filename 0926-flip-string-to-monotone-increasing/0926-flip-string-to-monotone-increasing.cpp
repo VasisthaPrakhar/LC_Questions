4f8232d22397e75da55fa22993ef089b4b021464
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-01-17 23:54:45 +0530

    Time: 21 ms (92.15%), Space: 9.9 MB (99.24%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string &s) {
-        int c0=0,c1=0,n=s.length(),ans=-1;
+        int c0=0,c1=0,n=s.length(),ans=0;
         for(int i=0;i<n;i++){
             if(s[i]=='0'){
                 if(c1>0){
                     c0++;
-                    ans=min(c1,c0);
+                    ans=c1>c0?c0:c1;
                 }
             }else{
                 c1++;
-                if(ans!=-1){
+                if(c0>0){
                     c0=ans;
                 }
             }
         }
-        return ans==-1?0:ans;
+        return ans;
     }
 };
