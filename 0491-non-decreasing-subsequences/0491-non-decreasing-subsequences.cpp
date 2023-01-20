e7892d8f8765a892373efc2e82ea7099918706df
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-01-20 15:12:27 +0530

    Time: 77 ms (68.80%), Space: 25.8 MB (47.95%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    set<vector<int>>s;//unordered nahi chala tha isme iss jagah pe particularly(unordered set does not work with vector<int>)
+    void dp(int n,int idx,int prev,vector<int>&a,vector<int>&nums){
+        if(idx>n){
+            return;
+        }
+        if(a.size()>=2){
+            s.insert(a);
+        }
+        for(int i=idx;i<n;i++){
+            if(nums[i]>=prev){
+                a.push_back(nums[i]);
+                dp(n,i+1,nums[i],a,nums);
+                a.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        vector<int>a;
+        dp(n,0,-101,a,nums);
+        for(auto x:s){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
