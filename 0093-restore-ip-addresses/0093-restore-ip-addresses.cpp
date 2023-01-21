c414a0a83ae14856b9392e82ef10cf58be21bcfc
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-01-21 18:40:11 +0530

    Time: 17 ms (6.05%), Space: 9.1 MB (5.21%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -6,18 +6,16 @@ public:
                 t.pop_back();
                 ans.push_back(t);
             }
-            //cout<<t<<endl;
             return;
         }
+        string x=t;
         for(int i=1;i<=min((n-idx),3);i++){
             string temp=s.substr(idx,i);
             if(i>1 && temp[0]=='0'){
                 continue;
             }
-            //cout<<temp<<endl;
             int z=stoi(temp);
             if(z<256){
-                string x=t;
                 t+=temp+".";
                 fun(s,idx+i,t,n,ans,k+1);
                 t=x;
