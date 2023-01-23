f9ead368f2466bb01fe749f513128241a52ccfc2
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-01-23 22:40:21 +0530

    Time: 410 ms (22.00%), Space: 72 MB (12.12%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    Solution() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+    
+    int findJudge(int n, vector<vector<int>>& t) {
+        unordered_map<int,vector<int>>adj,adj1;
+        int m=t.size();
+        if(m==0 && n==1){
+            return 1;
+        }
+        for(int i=0;i<m;i++){
+            adj[t[i][1]].push_back(t[i][0]);
+            adj1[t[i][0]].push_back(t[i][1]);
+        }
+        int flag=-1;
+        for(int i=1;i<=n;i++){
+            if(adj.count(i) && adj[i].size()==n-1){
+                if(adj1.count(i)==0){
+                    flag=i;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+};
