29ad1d21d1e4b72e84fe7f03d7bec16255f89667
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-02-08 02:46:52 +0530

    Time: 197 ms (49.58%), Space: 75.3 MB (6.69%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& a) {
+        int n=a.size();
+        if(n<=2){
+            return n;
+        }
+        int j=0,i=0,ans=2;
+        map<int,int>m;
+        while(j<n){
+            m[a[j]]++;
+            while(i<j && m.size()>2){
+                m[a[i]]--;
+                if(m[a[i]]==0){
+                    m.erase(a[i]);
+                }
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
