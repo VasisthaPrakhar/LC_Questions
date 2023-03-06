5d4f266b5e24b60ff9e55ade1a5f906bc15c74b8
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-03-06 22:42:17 +0530

    Time: 5 ms (63.62%), Space: 9.6 MB (38.05%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -5,13 +5,14 @@ public:
             return k;
         }
         int n=arr.size();
-        int beg=0,end=n;
-        while(beg<end){
-            int mid=(beg+end)>>1;
+        int beg=0,end=n-1,ans;
+        while(beg<=end){
+            int mid=(beg+end)/2;
             if(arr[mid]-(mid+1)<k){
+                //ans=beg;
                 beg=mid+1;
             }else{
-                end=mid;
+                end=mid-1;
             }
         }
         return beg+k;
