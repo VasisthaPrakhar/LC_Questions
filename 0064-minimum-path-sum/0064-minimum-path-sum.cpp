dea3edf924e065c055da11c13defdb812dc83d6e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-03-28 00:47:46 +0530

    Time: 10 ms (65.71%), Space: 9.5 MB (98.74%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,21 @@
+static const auto Initialize = []{
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    
+    int minPathSum(vector<vector<int>>& dp) {
+        int n=dp.size();
+        int m=dp[0].size();
+        for(int i=1;i<n;i++) dp[i][0]=dp[i-1][0]+dp[i][0];
+        for(int i=1;i<m;i++) dp[0][i]=dp[0][i-1]+dp[0][i];
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j]+=min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+};
