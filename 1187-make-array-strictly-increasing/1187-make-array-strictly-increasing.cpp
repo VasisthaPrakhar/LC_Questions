28c7577029bffb1dafc1cb01d475e3932cf5cc5e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-06-17 23:12:52 +0530

    Time: 1056 ms (9.30%), Space: 76.8 MB (19.19%) - LeetHub

---
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    map<int,map<int,int>>dp;
+    int fun(vector<int>& a, vector<int>& b,int n,int idx,int prev){
+        if(idx==n){
+            return 0;
+        }
+        if(dp.count(idx)){
+            if(dp[idx].count(prev)){
+                return dp[idx][prev];
+            }
+        }
+        //cout<<a[idx]<<" "<<prev<<endl;
+        auto f=upper_bound(b.begin(),b.end(),prev)-b.begin();
+        int res=2002;
+        if(f<b.size()){
+            res=fun(a,b,n,idx+1,b[f])+1;
+            //cout<<f;
+        }
+        if(prev<a[idx]){
+            res=min(res,fun(a,b,n,idx+1,a[idx]));
+        }
+        return dp[idx][prev]=res;
+    }
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        int n=arr1.size();
+        sort(arr2.begin(),arr2.end());
+        dp.clear();
+        int ans=fun(arr1,arr2,n,0,-1);
+        return ans>=2002?-1:ans;
+    }
+};
