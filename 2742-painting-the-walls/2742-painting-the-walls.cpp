8a5208492dd3a5dcd0eb96b55d4cdaf8eafaf9f8
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-06-20 22:37:36 +0530

    Time: 97 ms (99.34%), Space: 89.9 MB (74.20%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        int n=cost.size();
+        int dp[n+1][n+1];
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<=n;i++){
+            dp[0][i]=1e9;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                //if(j>time[i-1])
+                dp[i][j]=min(dp[i-1][j],dp[i-1][max(j-time[i-1]-1,0)]+cost[i-1]);
+                // else{
+                //     dp[i][j]=dp[i-1][j];
+                // }
+            }
+        }
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[n][n];
+    }
+};
