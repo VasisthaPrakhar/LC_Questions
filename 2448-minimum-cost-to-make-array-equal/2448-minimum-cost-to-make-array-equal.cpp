85a1d33ae5369b01e9aa1100865ee48bd4539797
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-06-21 21:35:12 +0530

    Time: 139 ms (20.80%), Space: 38.6 MB (55.11%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,41 @@
+typedef long long int ll;
+class Solution {
+public:
+   ll f(ll curr, ll n, vector<int>&a, vector<int>&c)
+    {
+        ll ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=(abs(curr-a[i]))*c[i];
+        }
+        return ans;
+    }
+    bool check(vector<int>&s, vector<int>&c){
+        int i=0,j=1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    long long minCost(vector<int>& a, vector<int>& c) {
+        ll n=a.size();
+        ll beg=-100000000, end=100000000;
+        check(a,c);
+        while(beg<end)
+        {
+            ll mid=(beg+end)/2;
+            ll sm=f(mid,n,a,c);
+            ll su=f(mid+1,n,a,c);
+            ll sl=f(mid-1,n,a,c);
+            if(sm<su && sm<sl)return sm;
+            if(su>sm)end=mid;
+            else beg=mid+1;
+        }
+        return f(end,n,a,c);
+    }
+};
