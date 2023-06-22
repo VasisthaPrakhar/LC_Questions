91314f6a056070e723bedba60e6d8ea0ee9c21de
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-06-22 20:58:14 +0530

    Time: 305 ms (5.34%), Space: 96.3 MB (5.73%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    // int fun(vector<int>& p,int h,int i, int &n,vector<vector<int>>&memo,int &fee){
-    //     if(i==n){
-    //         return 0;
-    //     }
-    //     if(memo[i][h]==0){
-    //         if(h){
-    //             memo[i][h]=max(-fee+p[i]+fun(p,0,i+1,n,memo,fee),fun(p,h,i+1,n,memo,fee));
-    //         }else{
-    //             memo[i][h]=max(-p[i]+fun(p,1,i+1,n,memo,fee),fun(p,h,i+1,n,memo,fee));
-    //         }
-    //     }
-    //     return memo[i][h];
-    // }
+    int fun(vector<int>& p,int h,int i, int &n,vector<vector<int>>&memo,int &fee){
+        if(i==n){
+            return 0;
+        }
+        if(memo[i][h]==0){
+            if(h){
+                memo[i][h]=max(-fee+p[i]+fun(p,0,i+1,n,memo,fee),fun(p,h,i+1,n,memo,fee));
+            }else{
+                memo[i][h]=max(-p[i]+fun(p,1,i+1,n,memo,fee),fun(p,h,i+1,n,memo,fee));
+            }
+        }
+        return memo[i][h];
+    }
     int maxProfit(vector<int>& p, int fee) {
         int n=p.size();
         if(n==0){
             return 0;
         }
-       //vector<vector<int>>memo(n+1,vector<int>(2,0));
-        int b=INT_MAX,s=0;
-        for(int i=0;i<n;i++){
-            b=min(b,p[i]-s);
-            s=max(s,-b+p[i]-fee);
-        }
-        return s;   
+       vector<vector<int>>memo(n+1,vector<int>(2,0));
+        return fun(p,0,0,n,memo,fee);   
     }
 };
