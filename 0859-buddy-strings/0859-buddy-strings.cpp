83a9a2db48631500ca2be9503ea34c1ad9e32cc5
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-07-03 20:15:57 +0530

    Time: 8 ms (20.95%), Space: 6.8 MB (85.37%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string t) {
+        int n=s.length();
+        int m=t.length(),f=0;
+        if(n!=m){return false;}
+        char a,b;
+        set<char>st;
+        for(int i=0;i<n;i++){
+            if(!f){
+                if(s[i]!=t[i]){
+                    a=s[i];
+                    b=t[i];
+                    f++;
+                }
+            }else{
+                if(s[i]!=t[i]){
+                    if(a!=t[i] || b!=s[i]){
+                        return false;
+                    }
+                    f++;
+                }
+            }
+            st.insert(s[i]);
+        }
+        if(s==t && st.size()<n){
+            return true;
+        }
+        if(f==2){
+            return true;
+        }
+        return false;
+    }
+};
