d0a35a0e6177cf0464be0ecb5cb3243cef5c70b1
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-07-04 21:51:37 +0530

    Time: 53 ms (6.10%), Space: 28.1 MB (8.78%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    typedef long long int ll;
+    vector<ll>fun(ll x){
+        vector<ll>ans;
+        while(x){
+            ans.push_back(x%2);
+            x/=2;
+        }
+        return ans;
+    }
+    int singleNumber(vector<int>& nums) {
+        vector<ll>a(32,0),b(32,0);
+        for(auto x:nums){
+            if(x>=0){
+                vector<ll>t=fun(x);
+                for(ll i=0;i<t.size();i++){
+                    a[i]+=t[i];
+                }
+            }else{
+                vector<ll>t=fun(abs(x));
+                for(ll i=0;i<t.size();i++){
+                    b[i]+=t[i];
+                }
+            }
+        }
+        int ans=0,ans1=0;
+        for(ll i=0;i<32;i++){
+            if(a[i]%3!=0){
+                ans+=1<<i;
+            }
+            if(b[i]%3!=0){
+                ans1+=1<<i;
+            }
+        }
+        if(ans1==INT_MIN){return ans1;}
+        return ans>ans1?ans:-ans1;
+    }
+};
