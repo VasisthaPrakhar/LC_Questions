0a3eccbf034d2b29102b3d1424e92510006ba061
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-07-07 22:04:46 +0530

    Time: 29 ms (59.12%), Space: 9.9 MB (95.61%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int fun(string &s, int k,char ch){
+        int n=s.length();
+        int ma=0,j=0,i=0,z=0;
+        while(j<n){
+            if(s[j]!=ch){
+                z++;
+            }
+            while(i<=j && z>k){
+                if(s[i]!=ch){
+                    z--;
+                }
+                i++;
+            }
+            ma=max(j-i+1,ma);
+            j++;
+        }
+        return ma;
+    }
+    int maxConsecutiveAnswers(string a, int k) {
+        return max(fun(a,k,'T'),fun(a,k,'F'));
+    }
+};
