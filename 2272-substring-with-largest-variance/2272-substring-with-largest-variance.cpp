fa72d9004ce514be0f6bb069e65a7c496cb88093
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-07-09 22:01:46 +0530

    Time: 167 ms (79.87%), Space: 6.9 MB (28.93%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int largestVariance(string s) {
+        int n=s.length(),ans=0;
+        for(int i=0;i<26;i++){
+            for(int j=0;j<26;j++){
+                if(i!=j){
+                    int h=0,l=0,f=0;
+                    for(int k=0;k<n;k++){
+                        if(s[k]-'a'==i){
+                            h++;
+                        }
+                        if(s[k]-'a'==j){
+                            l++;
+                        }
+                        if(l>0){
+                            ans=max(ans,h-l);
+                        }else{
+                            if(f){ans=max(ans,h-1);}
+                        }
+                        if(l>h){
+                            f=1;
+                            h=0,l=0;
+                            //k--;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
