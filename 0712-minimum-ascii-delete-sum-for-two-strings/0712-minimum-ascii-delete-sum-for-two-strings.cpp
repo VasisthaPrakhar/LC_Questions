0fe7b51627958fa6e7b5c4ea4d45697f84a1dedf
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-07-31 22:03:52 +0530

    Time: 7 ms (99.97%), Space: 7 MB (87.50%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -11,13 +11,7 @@ public:
             sum+=x;
         }
         int dp[n+1][m+1];
-        memset(dp,-1,sizeof(dp));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
+        memset(dp,0,sizeof(dp));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s1[i-1]==s2[j-1]){
