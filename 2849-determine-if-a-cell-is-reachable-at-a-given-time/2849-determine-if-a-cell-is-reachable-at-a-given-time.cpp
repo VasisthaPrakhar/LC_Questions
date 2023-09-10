ad1da32231ab707d4b63b96c694795101ef1cc4d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-09-10 15:19:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (33.33%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -3,12 +3,11 @@ public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
         int ma = abs(fx-sx)+abs(fy-sy);
         if(ma==0 && t-ma==1){
-            return 0;
+            return false;
         }
         if(t>=max(abs(fx-sx),abs(fy-sy))){
             return true;
         }
-        //&& t<=)
         return false;
     }
 };
