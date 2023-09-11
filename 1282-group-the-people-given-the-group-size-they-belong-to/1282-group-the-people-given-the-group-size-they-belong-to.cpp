5f9410332be78d6d51a1e3e3220254a4c442c410
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-09-11 21:45:48 +0530

    Time: 16 ms (25.17%), Space: 14.3 MB (7.90%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& g) {
+        int n=g.size();
+        vector<vector<int>>ans;
+        map<int,vector<int>>m;
+        for(int i=0;i<n;i++){
+            m[g[i]].push_back(i);
+            if(g[i]==m[g[i]].size()){
+                ans.push_back(m[g[i]]);
+                m.erase(g[i]);
+            }
+        }
+        return ans;
+    }
+};
