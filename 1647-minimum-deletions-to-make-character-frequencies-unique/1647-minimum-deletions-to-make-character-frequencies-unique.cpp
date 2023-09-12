86217a5cc1e7a5a2510c1263b4c20ac8d2e32268
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-09-12 21:35:56 +0530

    Time: 124 ms (14.75%), Space: 14.9 MB (98.34%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int minDeletions(string s) {
-        vector<int>m(26,0);
-        for(auto x:s){
-            m[x-'a']++;
+    int minDeletions(string &s) {
+        map<char,int>m;
+        for(auto &x:s){
+            m[x]++;
         }
         priority_queue<int>pq;
-        for(int i=0;i<26;i++){
-            if(m[i]>0)
-                pq.push(m[i]);
+        for(auto x:m){
+            pq.push(x.second);
         }
         int ans=0,prev=-1;
         while(!pq.empty()){
             int k=pq.top();
-            if(prev==pq.top()){
+            cout<<k<<" ";
+            if(k==prev){
+                ans++;
                 if(k>1)
                     pq.push(k-1);
-                ans++;
             }
             pq.pop();
             prev=k;
