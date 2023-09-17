8973f4be7bb5bd90090df643fe5374dd7fe65a55
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-09-17 17:31:55 +0530

    Time: 95 ms (18.18%), Space: 55.4 MB (18.18%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2861-maximum-number-of-alloys/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2861-maximum-number-of-alloys.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    typedef long long int ll;
+    bool fun(int n, int k, int budget, vector<vector<int>>& co, vector<int>& stock, vector<int>& cost,ll mid){
+         for(int i=0;i<k;i++){
+            long double s=0;
+            for(int j=0;j<n;j++){
+                ll req=mid*co[i][j]-stock[j];
+                if(req>0)
+                    s+=req*cost[j];    
+            }
+            if(s<=budget){
+                return true;
+            }
+         }
+        return false;
+    }
+    int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>>& co, vector<int>& stock, vector<int>& cost) {
+        ll beg=0,end=1e14,ans=0,mid=0,f=1;
+        while(beg<=end){
+            //cout<<beg<<" "<<end<<endl;
+            mid=(beg+end)/2;
+            if(fun(n,k,budget,co,stock,cost,mid)){
+                //cout<<mid<<" ";
+                ans=mid;
+                beg=mid+1;
+            }else{
+                end=mid-1;
+            }
+            
+        }
+        return ans;
+    }
+};
