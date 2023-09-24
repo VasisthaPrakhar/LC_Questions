66d8dc05683e690136d9970067c581120b1e95ad
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-09-24 22:14:32 +0530

    Time: 19 ms (20.18%), Space: 44.1 MB (5.53%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private: vector<vector<double>> init(double p,int qr){
+    vector<vector<double>>a(102,vector<double>(102,0));
+    a[0][0]=p;
+    for(int i=0;i<qr+1;i++){
+        for(int j=0;j<=i;j++){
+            if(a[i][j]>1){
+                a[i+1][j]+=(a[i][j]-1)/2.0;
+                a[i+1][j+1]+=(a[i][j]-1)/2.0;
+                a[i][j]=1;
+            }
+        }
+    }
+    return a;
+}
+public:
+    double champagneTower(int p, int qr, int qc) {
+        vector<vector<double>>a=init(p,qr);
+        // for(int i=0;i<101;i++){
+        //     for(int j=0;j<a[i].size();j++){
+        //         cout<<a[i][j]<<" ";
+        //         if(a[i][j]>1){
+        //             a[i][j]=1;
+        //         }
+        //     }
+        //     cout<<endl;
+        // }
+        return a[qr][qc];
+    }
+};
