f4e63887aace48e4d11f10b2906c5e2850df8c2e
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-10-22 23:28:58 +0530

    Time: 238 ms (30.77%), Space: 127.8 MB (61.54%) - LeetHub

--- 2910-minimum-number-of-groups-to-create-a-valid-assignment/2910-minimum-number-of-groups-to-create-a-valid-assignment.cpp
+++ 2910-minimum-number-of-groups-to-create-a-valid-assignment/2910-minimum-number-of-groups-to-create-a-valid-assignment.cpp
@@ -33,23 +33,14 @@ public:
         if(mi==n){
             return 1;
         }
-        int beg=2,end=mi+1,mid,ans=1;
-        // while(beg<=end){
-        //     mid=(beg+end)/2;
-        //     if(check(mid,freq)){
-        //         ans=mid;
-        //         beg=mid+1;
-        //     }else{
-        //         end=mid-1;
-        //     }
-        // }
+        int ans=1;
         for(int i=1;i<=mi+1;i++){
             if(check(i,freq)){
                 ans=i;
             }
         }
         int res=0;
-        cout<<ans<<" ";
+        //cout<<ans<<" ";
         for(auto x:freq){
             res+=ceil((double)x.second/(double)ans);
         }
