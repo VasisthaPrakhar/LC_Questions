eef8c30e9cc899ce91e7ce2877036d1d503e1fc9
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-10-29 22:14:48 +0530

    Time: 106 ms (100.00%), Space: 80.7 MB (100.00%) - LeetHub

--- 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -3,13 +3,16 @@ public:
     typedef long long int ll;
     long long minIncrementOperations(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<ll>dp(n,0);
-        dp[0]=max(0,k-nums[0]);
-        dp[1]=max(0,k-nums[1]);
-        dp[2]=max(0,k-nums[2]);
+        ll a,b,c;
+        a=max(0,k-nums[0]);
+        b=max(0,k-nums[1]);
+        c=max(0,k-nums[2]);
         for(int i=3;i<n;i++){
-            dp[i]=max(0,k-nums[i])+min({dp[i-1],dp[i-2],dp[i-3]});
+            ll mi=min({a,b,c});
+            a=b;
+            b=c;
+            c=max(0,k-nums[i])+mi;
         }
-        return min({dp[n-1],dp[n-2],dp[n-3]});
+        return min({a,b,c});
     }
 };
