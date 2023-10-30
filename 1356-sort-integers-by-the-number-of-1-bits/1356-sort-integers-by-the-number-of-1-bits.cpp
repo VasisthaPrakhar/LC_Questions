ab9423837c3732987444f34f849daf6df7e1617b
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-10-30 21:52:09 +0530

    Time: 15 ms (15.69%), Space: 10.4 MB (81.00%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sortByBits(vector<int>& arr) {
         sort(arr.begin(),arr.end(),[](int &a, int &b)->bool{
             int c=0,c1=0;
-            for(int i=0;i<16;i++){
+            for(int i=0;i<15;i++){
                 if(a & (1<<i)){
                     c++;
                 }
