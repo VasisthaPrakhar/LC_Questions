b2ad8112a6714a757b8b8d91be0b382a9f3f16a3
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-11-11 01:36:41 +0530

    Time: 671 ms (13.30%), Space: 160.8 MB (25.38%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -12,27 +12,22 @@ public:
     vector<int> restoreArray(vector<vector<int>>& a) {
         int n=a.size();
         vector<int>ans;
-        map<int,int>m;
-        for(auto x:a){
-            m[x[0]]++;
-            m[x[1]]++;
+        map<int,vector<int>>m;
+        for(int i=0;i<n;i++){
+            m[a[i][0]].push_back(a[i][1]);
+            m[a[i][1]].push_back(a[i][0]);
         }
         int st=-1;
         for(auto x:m){
-            if(x.second==1){
+            if(x.second.size()==1){
                 if(st==-1){
                     st=x.first;
                     break;
                 }
             }
         }
-        map<int,vector<int>>m1;
-        for(int i=0;i<n;i++){
-            m1[a[i][0]].push_back(a[i][1]);
-            m1[a[i][1]].push_back(a[i][0]);
-        }
         set<int>vis;
-        fun(m1,vis,st,ans);
+        fun(m,vis,st,ans);
         return ans; 
     }
 };
