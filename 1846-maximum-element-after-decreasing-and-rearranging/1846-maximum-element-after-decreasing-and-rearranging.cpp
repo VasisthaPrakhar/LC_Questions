51fcbde139af3057ac86adb98b1cfdad2c1e17ee
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-11-15 20:44:36 +0530

    Time: 86 ms (21.64%), Space: 51.6 MB (56.72%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -5,9 +5,7 @@ public:
         sort(arr.begin(),arr.end());
         for(int i=0;i<n;i++){
             if(i==0){
-                if(arr[i]>1){
-                    arr[i]=1;
-                }
+                arr[i]=1;
             }else{
                 if(arr[i]>arr[i-1]){
                     arr[i]=arr[i-1]+1;
