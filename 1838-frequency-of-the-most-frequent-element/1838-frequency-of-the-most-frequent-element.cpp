caf1ae70aa823dc3eae21de1d026b54c1254a2cf
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-11-18 22:50:52 +0530

    Time: 179 ms (30.22%), Space: 99.4 MB (25.44%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     typedef long long int ll;
     int maxFrequency(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1){return 1;}
         sort(nums.begin(),nums.end());
-        ll n=nums.size(),ans=0;
-        ll sum=0,j=0,i=0;
+        ll sum=0,j=0,i=0,ans=0;
         while(j<n){
             sum+=nums[j];
             while(i<j && (((j-i+1)*(ll)nums[j])-sum)>k){
