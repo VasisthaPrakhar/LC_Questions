649c7ef5eea9462fa503384c138b978c1a5b5a24
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-12-03 17:33:53 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (16.35%) - LeetHub

--- 2939-maximum-xor-product/2939-maximum-xor-product.cpp
+++ 2939-maximum-xor-product/2939-maximum-xor-product.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     typedef long long int ll;
-    int fun(ll n){
-        return (int)log2(n) + 1;
-    }
-    string fun1(ll k){
-        string s="";
-        //cout<<k;
-        while(k){
-            s=to_string(k%2)+s;
-            k/=2;
-        }
-        return s;
-    }
     int mod=1e9 + 7;
     int maximumXorProduct(long long a, long long b, int n) {
         ll x=0;
-        // a=a%mod;
-        // b=b%mod;
         for(ll i=n-1;i>=0;i--){
             ll k=1LL<<i;
             //cout<<k<<endl;
