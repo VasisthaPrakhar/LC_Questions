8d30644afa3e120436d3129a09452aaa3f724f86
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2023-12-04 23:17:32 +0530

    Time: 28 ms (5.22%), Space: 13.4 MB (5.80%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,34 +1,55 @@
+// class Solution {
+// public:
+//     string largestGoodInteger(string num) {
+//         int n=num.length();
+//         map<int,int>v;
+//         for(int i=0;i<3;i++){
+//             v[num[i]-'0']++;
+//         }
+//         int ans=-1;
+//         int j=3;
+//         if(v.size()==1){
+//             int a=v.begin()->first;
+//             ans=max(ans,a*100+a*10+a);
+//         }
+//         while(j<n){
+//             v[num[j]-'0']++;
+//             v[num[j-3]-'0']--;
+//             if(v[num[j-3]-'0']==0){
+//                 v.erase(num[j-3]-'0');
+//             }
+//             if(v.size()==1){
+//                 int a=v.begin()->first;
+//                 ans=max(ans,a*100+a*10+a);
+//             }
+//             j++;
+//         }
+//         if(ans==-1){
+//             return "";
+//         }else if(ans==0){
+//             return "000";
+//         }
+//         return to_string(ans);
+//     }
+// };
 class Solution {
 public:
+    bool check(string &s){
+        map<char,int>m;
+        for(auto x:s){
+            m[x]++;
+        }
+        return m.size()==1;
+    }
     string largestGoodInteger(string num) {
+        string ans="";
         int n=num.length();
-        map<int,int>v;
-        for(int i=0;i<3;i++){
-            v[num[i]-'0']++;
-        }
-        int ans=-1;
-        int j=3;
-        if(v.size()==1){
-            int a=v.begin()->first;
-            ans=max(ans,a*100+a*10+a);
-        }
-        while(j<n){
-            v[num[j]-'0']++;
-            v[num[j-3]-'0']--;
-            if(v[num[j-3]-'0']==0){
-                v.erase(num[j-3]-'0');
+        for(int i=0;i<n-2;i++){
+            string temp=num.substr(i,3);
+            if(check(temp)){
+                ans=max(ans,temp);
             }
-            if(v.size()==1){
-                int a=v.begin()->first;
-                ans=max(ans,a*100+a*10+a);
-            }
-            j++;
         }
-        if(ans==-1){
-            return "";
-        }else if(ans==0){
-            return "000";
-        }
-        return to_string(ans);
+        return ans;
     }
 };
