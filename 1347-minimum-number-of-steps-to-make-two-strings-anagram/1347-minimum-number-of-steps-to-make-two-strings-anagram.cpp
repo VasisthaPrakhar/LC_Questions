ee7e5e8b021971de0105113e22efc6cf10058cef
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-01-13 23:07:06 +0530

    Time: 74 ms (43.52%), Space: 14.5 MB (99.49%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int minSteps(string s, string t) {
-        map<char,int>m;
-        for(auto x:t){
+    int minSteps(string &s, string &t) {
+        unordered_map<char,int>m;
+        for(auto &x:t){
             m[x]++;
         }
-        for(auto x:s){
+        for(auto &x:s){
             m[x]--;
         }
         int ans=0;
-        for(auto x:m){
+        for(auto &x:m){
             ans+=abs(x.second);
         }
         return ans/2;;
