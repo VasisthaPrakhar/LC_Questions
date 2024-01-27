3c50878126eb482d742f33e009e0970bf5ff513c
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-01-28 02:58:32 +0530

    Time: 24 ms (41.04%), Space: 8.4 MB (7.03%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -16,7 +16,7 @@ public:
                 dp[i]=((dp[i]%mod + dp[i-d]%mod)+mod)%mod;
                 a[i]=dp[i-d]%mod;
             }
-            cout<<i+1<<" "<<dp[i]<<" "<<a[i]<<endl;
+            //cout<<i+1<<" "<<dp[i]<<" "<<a[i]<<endl;
         }
         return dp[n-1]<0?0:dp[n-1];
     }
