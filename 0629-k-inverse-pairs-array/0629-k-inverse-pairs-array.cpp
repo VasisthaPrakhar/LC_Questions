38341b6af335f37a52eaf0c25212148961333559
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-01-28 00:52:26 +0530

    Time: 2880 ms (5.49%), Space: 11.7 MB (39.56%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    typedef long long int ll;
+    int dp[1001][1001];
+    int mod=1e9 + 7;
+    int fun(int n, int k){
+        if(k==0){
+            return 1;
+        }
+        if(dp[n][k]!=-1){return dp[n][k];}
+        ll res=0;
+        for(int i=0;i<n;i++){
+            if(k-i>=0){
+                res = (res%mod + fun(n-1,k-i)%mod)%mod;
+            }
+        }
+        return dp[n][k]=res%mod;
+    }
+    int kInversePairs(int n, int k) {
+        memset(dp,-1,sizeof(dp));
+        return fun(n,k);
+    }
+};
