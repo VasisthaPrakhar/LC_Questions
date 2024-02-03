6e708e4c75df9ff67d63a658b886a37d94258290
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-02-04 00:01:16 +0530

    Time: 444 ms (5.09%), Space: 10.6 MB (28.35%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -3,7 +3,7 @@ public:
     typedef long long int ll;
     int dp[501];
     ll fun(vector<int> &a, int idx, int n,int k){
-        if(idx<0 || idx>=n){
+        if(idx>=n){
             return 0;
         }
         if(dp[idx]!=-1){
@@ -13,14 +13,14 @@ public:
         ll res=0;
         while(l>0){
             ll z=0,c=0;
-            for(int i=idx-l+1;i<=idx;i++){
-                if(i>=0){
+            for(int i=idx+l-1;i>=idx;i--){
+                if(i<n){
                     z=max(z,(ll)a[i]);
                     c++;
                 }
             }
             //cout<<l<<" "<<z<<" "<<idx<<endl;
-            res=max(res, fun(a,idx-l,n,k) + z*c);
+            res=max(res, fun(a,idx+l,n,k) + z*c);
             l--;
         }
         return dp[idx]=res;
@@ -28,6 +28,6 @@ public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
         memset(dp,-1,sizeof(dp));
-        return fun(arr,n-1,n,k);
+        return fun(arr,0,n,k);
     }
 };
