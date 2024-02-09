b99db185fc26bc696b268079d03d59bab6d2fcd3
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-02-10 00:57:38 +0530

    Time: 45 ms (18.32%), Space: 20.4 MB (5.01%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -39,7 +39,7 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<int>ans;
-        map<int,set<int>>m;
+        unordered_map<int,set<int>>m;
         for(int i=0;i<n;i++){
             m[i].insert(nums[i]);
         }
