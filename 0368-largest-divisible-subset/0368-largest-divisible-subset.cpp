e3792469f67123b742765c284b6981ffcc4780e7
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-02-10 00:57:09 +0530

    Time: 65 ms (10.17%), Space: 19.9 MB (5.01%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -26,14 +26,46 @@ public:
         return m[beg];
     }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        n=nums.size();
+        // n=nums.size();
+        // sort(nums.begin(),nums.end());
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     vector<int>ma=fun(nums,i);
+        //     if(ma.size()>ans.size()){
+        //         ans=ma;
+        //     }
+        // }
+        // return ans;
+        int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<int>ans;
+        map<int,set<int>>m;
         for(int i=0;i<n;i++){
-            vector<int>ma=fun(nums,i);
-            if(ma.size()>ans.size()){
-                ans=ma;
+            m[i].insert(nums[i]);
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(nums[j]%nums[i]==0){
+                    if(m[i].size() < m[j].size()+1){
+                        m[i].clear();
+                        m[i].insert(nums[i]);
+                        for(auto x:m[j]){
+                            m[i].insert(x);
+                        }
+                    }
+                }
             }
+        }
+        for(auto x:m){
+            //cout<<x.first<<": ";
+            if(x.second.size()>ans.size()){
+                vector<int>a;
+                for(auto y:x.second){
+                    a.push_back(y);
+                }
+                ans=a;
+            }
+            //cout<<endl;
         }
         return ans;
     }
