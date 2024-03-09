81f797f6533ef964eda43529bcbd6494191df519
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-03-09 22:26:39 +0530

    Time: 116 ms (20.28%), Space: 69 MB (18.41%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -3,8 +3,8 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int>mp;
         int ans=INT_MAX;
-        for(auto x:nums1){mp[x]++;}
-        for(auto x:nums2){
+        for(auto &x:nums1){mp[x]++;}
+        for(auto &x:nums2){
             if(mp.find(x)!=mp.end()){
                 ans=min(ans,x);
             }
