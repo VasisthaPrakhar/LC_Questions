7152feb726bbe7bf01d39b30090fdf6b63662c32
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-03-24 23:09:46 +0530

    Time: 628 ms (20.00%), Space: 199 MB (60.00%) - LeetHub

---
+++ 3092-most-frequent-ids/3092-most-frequent-ids.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    typedef long long int ll;
+    vector<long long> mostFrequentIDs(vector<int>& nums, vector<int>& freq) {
+        int n=nums.size();
+        map<ll,ll>m;
+        map<ll,set<ll>,greater<ll>>mp;
+        vector<ll>ans(n,0);
+        for(int i=0;i<n;i++){
+            if(mp.find(m[nums[i]])!=mp.end()){
+                mp[m[nums[i]]].erase(nums[i]);
+                if(mp[m[nums[i]]].size()==0){
+                    mp.erase(m[nums[i]]);
+                }
+            }
+            // for(auto x:m){
+            //     cout<<x.first<<" "<<x.second<< "||";
+            // }
+            // cout<<endl;
+            m[nums[i]]+=freq[i];
+            if(m[nums[i]]>0){
+                mp[m[nums[i]]].insert(nums[i]);
+            }
+            if(mp.size()){
+                ans[i]=mp.begin()->first;
+            }
+        }
+        return ans;
+    }
+};
