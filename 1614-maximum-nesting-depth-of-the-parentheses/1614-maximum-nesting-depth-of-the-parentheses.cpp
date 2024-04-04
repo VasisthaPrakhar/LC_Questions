d60e119764c4fd08887c4a6b15c31f59a5827d87
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-04-04 20:52:16 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (96.44%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,14 +3,13 @@ public:
     int maxDepth(string s) {
         int n=s.length();
         if(n<=1){return 0;}
-        int ans=0;
-        stack<char>st;
+        int ans=0,c=0;
         for(int i=0;i<n;i++){
             if(s[i]=='('){
-                st.push(s[i]);
+                c++;
             }else if(s[i]==')'){
-                ans=max(ans,(int)st.size());
-                st.pop();
+                ans=max(ans,c);
+                c--;
             }
         }
         return ans;
