59d08c26b45551ca1acd2c4b51f5f4ddc5cb1c81
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-04-07 13:13:58 +0530

    Time: 12 ms (5.26%), Space: 18.7 MB (7.00%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    bool fun(int idx, int n, string &s, int k, unordered_map<int,map<int,bool>>&m){
+        if(idx>=n){
+            if(k==0){return true;}
+            return false;
+        }
+        if(m.find(idx)!=m.end()){
+            if(m[idx].find(k) != m[idx].end()){
+                return m[idx][k];
+            }
+        }
+        bool res=false;
+        if(s[idx]=='('){
+            res|=fun(idx+1,n,s,k+1,m);
+        }else if(s[idx]==')'){
+            if(k>0)
+                res|=fun(idx+1,n,s,k-1,m);
+        }else{
+            res|=fun(idx+1,n,s,k+1,m);
+            if(k>0){
+                res|=fun(idx+1,n,s,k-1,m);
+            }
+            res|=fun(idx+1,n,s,k,m);
+        }
+        return m[idx][k] = res;
+    }
+    bool checkValidString(string &s) {
+        int n=s.length();
+        unordered_map<int,map<int,bool>>m;
+        return fun(0,n,s,0,m);
+    }
+};
