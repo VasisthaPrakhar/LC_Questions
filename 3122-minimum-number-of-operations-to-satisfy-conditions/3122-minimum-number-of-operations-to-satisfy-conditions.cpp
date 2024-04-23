e108c134404dae5442cacd06c0e25b2eb4ba542b
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-04-23 20:40:49 +0530

    Time: 510 ms (11.62%), Space: 150.1 MB (5.47%) - LeetHub

---
+++ 3122-minimum-number-of-operations-to-satisfy-conditions/3122-minimum-number-of-operations-to-satisfy-conditions.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    unordered_map<int,unordered_map<int,int>>dp;
+    int fun(int idx,int n,int num,unordered_map<int,map<int,int>>&a,int prev){
+        if(idx>=n){
+            return 0;
+        }
+        if(dp.find(idx)!=dp.end()){
+            if(dp[idx].find(prev)!=dp[idx].end()){
+                return dp[idx][prev];
+            }
+        }
+        int res=INT_MAX;
+        res=min(res,num+fun(idx+1,n,num,a,-1));
+        for(auto x:a[idx]){
+            if(x.first != prev){
+                res=min(res,(num-x.second) + fun(idx+1,n,num,a,x.first));
+            }
+        }
+        // if(res==INT_MAX){
+        //     res=min(res,num+fun(idx+1,n,num,a,-1));
+        // }
+        return dp[idx][prev]=res;
+    }
+    int minimumOperations(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        unordered_map<int,map<int,int>>mp;
+        for(int j=0;j<m;j++){
+            map<int,int>mp1;
+            for(int i=0;i<n;i++){
+                mp1[grid[i][j]]++;
+            }
+            mp[j]=mp1;
+        }
+        dp.clear();
+        return fun(0,m,n,mp,-1);
+    }
+};
