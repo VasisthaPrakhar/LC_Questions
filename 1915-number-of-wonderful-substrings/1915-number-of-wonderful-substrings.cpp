420b324a879a56f49c22134739cee81e76b1bd16
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-04-30 22:12:22 +0530

    Time: 340 ms (53.89%), Space: 18.8 MB (49.10%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -12,14 +12,17 @@ public:
         ll a=0,ans=0;
         for(int i=0;i<n;i++){
             a=a^(1<<(w[i]-'a'));
-            for(auto &x:m){
-                if(fun(x.first ^ a) || x.first==a){
-                    //cout<<i<<" "<<x.first<<" "<<a<<endl;
-                    ans+=x.second;
-                }
-            }
+            // for(auto &x:m){
+            //     if(fun(x.first ^ a) || x.first==a){
+            //         //cout<<i<<" "<<x.first<<" "<<a<<endl;
+            //         ans+=x.second;
+            //     }
+            // }
             //cout<<ans<<endl;
-            m[a]++;
+            ans+=m[a]++;
+            for(int i=0;i<10;i++){
+                ans+=m[a^(1<<i)];
+            }
         }
         return ans;
     }
