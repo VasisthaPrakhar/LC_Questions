4ed81526b535ab241cde145b7b945b0c9bfa6182
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-05-03 22:25:27 +0530

    Time: 3 ms (39.03%), Space: 8.1 MB (5.03%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,38 +1,74 @@
 class Solution {
 public:
-    int compareVersion(string v1, string v2) {
-        int c=0;
-        stringstream ss(v1),ss1(v2);
-        string token,token1;
-        while(getline(ss,token,'.') && getline(ss1,token1,'.')){
-            long long int a=stoll(token),b=stoll(token1);
-            if(a<b){
-                return -1;
-            }else if(a>b){
-                return 1;
+    int fun(string &v1, string &v2){
+        int n=v1.length();
+        int m=v2.length();
+        string a="";
+        int i,j;
+        for( i=0,j=0;i<n;i++){
+            if(v1[i]>='0'){
+                a.push_back(v1[i]);
             }
-            c++;
-        }
-        ss.clear(),ss1.clear();
-        stringstream ss2(v1),ss3(v2);
-        while(c--){
-            getline(ss2,token,'.');
-            getline(ss3,token1,'.');
-        }
-        cout<<token<<" "<<token1<<" ";
-        while(getline(ss2,token,'.')){
-            long long int a=stoll(token);
-            if(a>0){
-                return 1;
+            if(v1[i]=='.'){
+                string b="";
+                for(;j<m;j++){
+                    if(v2[j]=='.'){
+                        j++;
+                        break;
+                    }
+                    if(v2[j]>='0'){
+                        b.push_back(v2[j]);
+                    }
+                }
+                if(a.size()==0){a.push_back('0');}
+                if(b.size()==0){b.push_back('0');}
+                //cout<<a<<" "<<b<<endl;
+                long long int c=stoll(a);
+                long long int d=stoll(b);
+                if(c<d){
+                    return -1;
+                }
+                if(c>d){
+                    return 1;
+                }
+                a="";
             }
         }
-        while(getline(ss3,token1,'.')){
-            long long int a=stoll(token1);
-            if(a>0){
-                return -1;
+       // if(a.size()==0){a.push_back('0');}
+        string b="";
+        for(;j<m;j++){
+            if(v2[j]=='.'){
+                j++;
+                break;
+            }
+            if(v2[j]>='0'){
+                b.push_back(v2[j]);
             }
         }
-        ss2.clear(),ss3.clear();
+        if(a.size()==0){a.push_back('0');}
+        if(b.size()==0){b.push_back('0');}
+        //cout<<a<<" "<<b<<endl;
+        long long int c=stoll(a);
+        long long int d=stoll(b);
+        if(c<d){
+            return -1;
+        }
+        if(c>d){
+            return 1;
+        }
         return 0;
+    }
+    int compareVersion(string &v1, string &v2) {
+        if(fun(v1,v2)==0){
+            if(fun(v2,v1)==-1){
+                return 1;
+            }else if(fun(v2,v1)==1){
+                return -1;
+            }else{
+                return 0;
+            }
+        }else{
+            return fun(v1,v2);
+        }
     }
 };
