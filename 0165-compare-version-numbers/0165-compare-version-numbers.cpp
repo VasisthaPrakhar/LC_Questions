103e1af49c9dadf8c70ea1a970983a646510891f
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-05-03 22:06:35 +0530

    Time: 3 ms (39.03%), Space: 7.8 MB (19.65%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int c=0;
+        stringstream ss(v1),ss1(v2);
+        string token,token1;
+        while(getline(ss,token,'.') && getline(ss1,token1,'.')){
+            long long int a=stoll(token),b=stoll(token1);
+            if(a<b){
+                return -1;
+            }else if(a>b){
+                return 1;
+            }
+            c++;
+        }
+        ss.clear(),ss1.clear();
+        stringstream ss2(v1),ss3(v2);
+        while(c--){
+            getline(ss2,token,'.');
+            getline(ss3,token1,'.');
+        }
+        cout<<token<<" "<<token1<<" ";
+        while(getline(ss2,token,'.')){
+            long long int a=stoll(token);
+            if(a>0){
+                return 1;
+            }
+        }
+        while(getline(ss3,token1,'.')){
+            long long int a=stoll(token1);
+            if(a>0){
+                return -1;
+            }
+        }
+        ss2.clear(),ss3.clear();
+        return 0;
+    }
+};
