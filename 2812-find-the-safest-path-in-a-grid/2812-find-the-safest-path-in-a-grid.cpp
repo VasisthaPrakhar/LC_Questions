5b6e9a3c714ceb5d1919e37b78bd43f6d2c856fa
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-05-15 23:38:00 +0530

    Time: 626 ms (43.77%), Space: 163.9 MB (53.67%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -37,51 +37,25 @@ public:
         }
         bfs(q,n,a);
         vis.clear();
-        // queue<pair<int,int>>q;
-        // q.push({0,0});
-        // int ans=0;
-        // while(!q.empty()){
-        //     auto node=q.front();
-        //     int x=node.first;
-        //     int y=node.second;
-        //     q.pop();
-        //     if(x==n-1 && y==n-1){
-        //         ans=max(a[x][y],ans);
-        //         continue;
-        //     }
-        //     for(int i=0;i<4;i++){
-        //         int nr=node.first+dr[i];
-        //         int nc=node.second+dc[i];
-        //         if(nr<0 || nr>=n || nc<0 || nc>=n || a[nr][nc]==0){continue;}
-        //         if(vis.find({nr,nc})!=vis.end()){
-        //             a[nr][nc]=max(a[nr][nc],a[x][y]);
-        //         }else{
-        //             a[nr][nc]=min(a[nr][nc],a[x][y]);
-        //             vis.insert({nr,nc});
-        //         }
-        //         q.push({nr,nc});
-        //     }
-        //     a[x][y]=0;
-        // }
         priority_queue<pair<int,pair<int,int>>> pq;
         pq.push({a[0][0], {0,0}});
 
         while(!pq.empty()){
-            auto temp = pq.top().second;
-            auto safe = pq.top().first;
+            auto node = pq.top().second;
+            auto val = pq.top().first;
+            int x=node.first;
+            int y=node.second;
             pq.pop();
 
-            if(temp.first == n - 1 && temp.second == n - 1) return safe;
-            vis.insert({temp.first,temp.second});
+            if(x==n-1 && y==n-1) return val;
+            vis.insert({x,y});
             for(int i = 0; i < 4; i++) {
-                int x = temp.first + dr[i];
-                int y = temp.second + dc[i];
-
-                if(x >= 0 && x < n && y >= 0 && y < n && vis.find({x,y})==vis.end()){
-                    int s = min(safe, a[x][y]);
-                    pq.push({s, {x, y}});
-                    vis.insert({x,y});
-                }
+                int nr = x + dr[i];
+                int nc = y + dc[i];
+                if(nr < 0 || nr >= n || nc < 0 || nc >= n || vis.find({nr,nc})!=vis.end()){continue;}
+                int k = min(val, a[nr][nc]);
+                pq.push({k, {nr, nc}});
+                vis.insert({nr,nc});
             }
         }
 
