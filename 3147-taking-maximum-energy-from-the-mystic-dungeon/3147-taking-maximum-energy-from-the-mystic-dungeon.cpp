6bee952d3a9e9ff5f808c85c2dfddc05aea2f8e9
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-05-16 23:40:11 +0530

    Time: 139 ms (74.33%), Space: 149.6 MB (43.62%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -3,16 +3,16 @@ public:
     int maximumEnergy(vector<int>& a, int k) {
         int n=a.size();
         vector<int>dp(n);
+        int ma=INT_MIN;
         for(int i=0;i<n;i++){
             if(i-k<0){
                 dp[i]=a[i];
             }else{
                 dp[i]=max(a[i],a[i]+dp[i-k]);
             }
-        }
-        int ma=INT_MIN;
-        for(int i=n-1;i>+0 && k;i--,k--){
-            ma=max(dp[i],ma);
+            if(i+k>=n){
+                 ma=max(dp[i],ma);
+            }
         }
         return ma;
     }
