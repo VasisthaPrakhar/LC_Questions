fae30d874e0bcfa22fef2f8c1277f6cd08bf6282
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-05-16 22:37:04 +0530

    Time: 160 ms (26.51%), Space: 68.1 MB (5.47%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.cpp
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.cpp
@@ -3,17 +3,13 @@ public:
     int maxScore(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        map<int,vector<int>>mr;
-        vector<vector<int>>mc(n,vector<int>(m,INT_MIN));
+        vector<vector<int>>mc(n,vector<int>(m)),mr(n,vector<int>(m));
         for(int i=0;i<n;i++){
-            vector<int>suf;
             int ma=INT_MIN;
             for(int j=m-1;j>=0;j--){
                 ma=max(grid[i][j],ma);
-                suf.push_back(ma);
+                mr[i][j]=ma;
             }
-            reverse(suf.begin(),suf.end());
-            mr[i]=suf;
         }
         for(int j=0;j<m;j++){
             int ma=INT_MIN;
@@ -22,13 +18,6 @@ public:
                 mc[i][j]=ma;
             }
         }
-        // for(auto x:mc){
-        //    // cout<<x.first<<" : ";
-        //     for(auto y:x){
-        //         cout<<y<<" ; ";
-        //     }
-        //     cout<<endl;
-        // }
         int ans=INT_MIN;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -40,7 +29,6 @@ public:
                     b=mc[i][j+1];
                 }
                 if(a>-1 || b>-1){
-                    //cout<<i<<" "<<j <<" "<<grid[i][j]<<" "<<max(a,b)<<endl;
                     ans=max(ans,max(a,b)-grid[i][j]);
                 }
             }
