97bfc6a5986f45c8a32bc329359434c61003ffe3
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-05-23 21:57:50 +0530

    Time: 116 ms (69.63%), Space: 50.4 MB (6.41%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    
-    int fun(int idx,int n,vector<int>&nums,int k,unordered_map<int,int>&a){
+    unordered_map<int,map<vector<int>,int>>dp;
+    int fun(int idx,int n,vector<int>&nums,int k,vector<int>&a){
         if(idx>=n){
-            if(!a.empty())
-                return 1;
-            return 0;
+            return 1;
         }
         int res=0;
-        if(a.find(nums[idx]+k)==a.end() && a.find(nums[idx]-k)==a.end()){
+        if((a[nums[idx]+k]==0 && (nums[idx]-k)<0) || (a[nums[idx]+k]==0 && a[nums[idx]-k]==0)){
             a[nums[idx]]++;
             res+=fun(idx+1,n,nums,k,a);
             a[nums[idx]]--;
-            if(a[nums[idx]]==0){
-                a.erase(nums[idx]);
-            }
         }
         res +=fun(idx+1,n,nums,k,a);
         return res;
     }
     int beautifulSubsets(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,int>m;
-        return fun(0,n,nums,k,m);
+        vector<int>a(2002,0);
+        dp.clear();
+        return fun(0,n,nums,k,a)-1;
     }
 };
