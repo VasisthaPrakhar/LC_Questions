f0d107a17ccc8b7935eef52e4767c1a09eb5fc77
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-06-05 22:49:25 +0530

    Time: 7 ms (62.84%), Space: 12 MB (31.97%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& w) {
+        int n=w.size();
+        vector<int>a(26,INT_MAX);
+        for(auto x:w){
+            vector<int>b(26,0);
+            for(auto y:x){
+                b[y-'a']++;
+            }
+            for(int i=0;i<26;i++){
+                a[i]=min(b[i],a[i]);
+            }
+        }
+        vector<string>ans;
+        for(int i=0;i<26;i++){
+            if(a[i]!=INT_MAX){
+                for(int j=0;j<a[i];j++){
+                    string t;
+                    t.push_back(i+'a');
+                    ans.push_back(t);
+                }
+            }
+        }
+        return ans;
+    }
+};
