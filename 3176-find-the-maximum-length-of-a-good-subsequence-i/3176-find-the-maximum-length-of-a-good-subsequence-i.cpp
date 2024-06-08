223e3ee849a59fd436ea1a711c15a4c4f43ac63a
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-06-09 00:13:27 +0530

    Time: 1490 ms (12.50%), Space: 50.8 MB (12.50%) - LeetHub

---
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    //unordered_map<string,int>dp;
+    int dp[501][26][502];
+    int fun(int idx,int k,vector<int>&a, int n,int prev){
+        if(idx>=n){
+            return 0;
+        }
+        if(dp[idx][k][prev]!=-1){
+            return dp[idx][k][prev];
+        }
+        int res=0,x=0,y=0;
+        if(prev==0){
+            x=1+fun(idx+1,k,a,n,a[idx]);
+            y=fun(idx+1,k,a,n,prev);
+        }else{
+            if(a[idx]!=prev){
+                if(k>0)
+                    x=1+fun(idx+1,k-1,a,n,a[idx]);   
+            }else{
+                x=1+fun(idx+1,k,a,n,a[idx]);
+            }
+            y=fun(idx+1,k,a,n,prev);
+        }
+        res=max(x,y);
+        return dp[idx][k][prev]=res;
+    }
+    int maximumLength(vector<int>& nums, int k) {
+        int n=nums.size();
+        map<int,vector<int>>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]].push_back(i);
+        }
+        int j=1;
+        for(auto &x:mp){
+            for(auto &y:x.second){
+                nums[y]=j;
+            }
+            j++;
+        }
+        memset(dp,-1,sizeof(dp));
+        return fun(0,k,nums,n,0);
+    }
+};
