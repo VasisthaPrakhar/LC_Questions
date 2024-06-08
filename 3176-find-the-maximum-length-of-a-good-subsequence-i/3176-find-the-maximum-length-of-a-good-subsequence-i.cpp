4c51bc427b2456d4fba1e00a453c4bb7e90a25d4
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-06-09 00:14:41 +0530

    Time: 1383 ms (12.50%), Space: 50.8 MB (12.50%) - LeetHub

--- 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -27,7 +27,7 @@ public:
     }
     int maximumLength(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,vector<int>>mp;
+        map<int,vector<int>>mp;
         for(int i=0;i<n;i++){
             mp[nums[i]].push_back(i);
         }
