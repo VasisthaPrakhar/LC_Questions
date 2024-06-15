fd1e18923c844f4c515470325b66968e595502c6
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-06-15 13:52:25 +0530

    Time: 112 ms (80.92%), Space: 69 MB (94.08%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        set<int>s;
-        for(int i=0;i<100001;i++){s.insert(i);}
-        for(auto x:nums){
-            if(s.find(x)!=s.end())
-                s.erase(x);
-        }
-        map<int,int>m;
-        int ans=0,k=100001;
-        for(auto x:nums){
-            m[x]++;
-            if(m[x]>1){
-                auto f=s.lower_bound(x);
-                if(f==s.end()){
-                    ans+=k-x;
-                    k++;
-                }else{
-                    ans+=*f-x;
-                    s.erase(f);
-                }
-                m[x]--;
-            }
+        // set<int>s;
+        // for(int i=0;i<100001;i++){s.insert(i);}
+        // for(auto x:nums){
+        //     if(s.find(x)!=s.end())
+        //         s.erase(x);
+        // }
+        // map<int,int>m;
+        // int ans=0,k=100001;
+        // for(auto x:nums){
+        //     m[x]++;
+        //     if(m[x]>1){
+        //         auto f=s.lower_bound(x);
+        //         if(f==s.end()){
+        //             ans+=k-x;
+        //             k++;
+        //         }else{
+        //             ans+=*f-x;
+        //             s.erase(f);
+        //         }
+        //         m[x]--;
+        //     }
+        // }
+        // return ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int ans=0,c=0;
+        for(int i=0;i<n;i++){
+            c=max(nums[i],c);
+            ans+=c-nums[i];
+            c++;
         }
         return ans;
     }
