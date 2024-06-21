4426068a67be173ed45b335044ec10e8dfaf0167
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-06-21 21:20:32 +0530

    Time: 23 ms (66.42%), Space: 34.4 MB (16.52%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& c, vector<int>& g, int m) {
         int n=c.size();
-        int s=0,s1=0,ma=0,l=-1,r=-1,res=0;
+        int s=0,s1=0;
         for(int i=0;i<m;i++){
             s+=c[i];
             if(g[i]==0){
                 s1+=c[i];
             }
         }
-        if((s-s1)>ma){
-            ma=(s-s1),res=s;
-            l=0,r=m-1;
-        }
-        int j=m,i=0;
+        int j=m,i=0,l=0,r=m-1,res=s,ma=s-s1;
         while(j<n){
             if(g[j]==0){
                 s1+=c[j];
             }
             if(g[i]==0){s1-=c[i];}
             s+=c[j],s-=c[i];
-            //cout<<s<<" "<<s1;
             if((s-s1)>ma){
                 res=s;
                 ma=(s-s1);
@@ -28,7 +23,6 @@ public:
             }
             j++,i++;
         }
-        //cout<<l<<" "<<r<<" "<<res<<endl;
         for(int i=0;i<l;i++){
             if(g[i]==0){res+=c[i];}
         }
