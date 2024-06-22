e7f630e61c2f230f3c07d9e9ae226e7b6a999680
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-06-22 17:41:15 +0530

    Time: 99 ms (43.80%), Space: 70 MB (36.89%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        int j=0,i=0,ans=0,z=0;
-        map<int,int>m;
+    int fun(vector<int>&a,int k,int &n){
+        int j=0,i=0,s=0,ans=0;
         while(j<n){
-            if(nums[j]%2){
-                z++;
-                m[j]++;
-            }
-            while(z>k && i<j){
-                if(nums[i]%2){
-                    z--;
-                    m[i]--;
-                    if(m[i]==0){
-                        m.erase(i);
-                    }
-                }
+            s+=(a[j]%2);
+            while(s>k && i<=j){
+                s-=(a[i]%2);
                 i++;
             }
-            if(z==k){
-                ans+=m.begin()->first-i+1;
-            }
+            ans+=j-i+1;
             j++;
         }
         return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
+        // int j=0,i=0,ans=0,z=0;
+        // map<int,int>m;
+        // while(j<n){
+        //     if(nums[j]%2){
+        //         z++;
+        //         m[j]++;
+        //     }
+        //     while(z>k && i<j){
+        //         if(nums[i]%2){
+        //             z--;
+        //             m[i]--;
+        //             if(m[i]==0){
+        //                 m.erase(i);
+        //             }
+        //         }
+        //         i++;
+        //     }
+        //     if(z==k){
+        //         ans+=m.begin()->first-i+1;
+        //     }
+        //     j++;
+        // }
+        // return ans;
+        return fun(nums,k,n)-fun(nums,k-1,n);
     }
 };
