df2b8a1f02d96216b936f6afc67e1581eee2a2f6
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-06-22 17:51:04 +0530

    Time: 93 ms (67.32%), Space: 70.1 MB (36.89%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int fun(vector<int>&a,int k,int &n){
-        int j=0,i=0,s=0,ans=0;
-        while(j<n){
-            s+=(a[j]%2);
-            while(s>k && i<=j){
-                s-=(a[i]%2);
-                i++;
-            }
-            ans+=j-i+1;
-            j++;
-        }
-        return ans;
-    }
+    // int fun(vector<int>&a,int k,int &n){
+    //     int j=0,i=0,s=0,ans=0;
+    //     while(j<n){
+    //         s+=(a[j]%2);
+    //         while(s>k && i<=j){
+    //             s-=(a[i]%2);
+    //             i++;
+    //         }
+    //         ans+=j-i+1;
+    //         j++;
+    //     }
+    //     return ans;
+    // }
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n=nums.size();
         // int j=0,i=0,ans=0,z=0;
@@ -38,6 +38,21 @@ public:
         //     j++;
         // }
         // return ans;
-        return fun(nums,k,n)-fun(nums,k-1,n);
+        auto p = [&] (int z)
+        {
+            int j=0,i=0,s=0,ans=0;
+            while(j<n){
+                s+=(nums[j]%2);
+                while(s>z && i<=j){
+                    s-=(nums[i]%2);
+                    i++;
+                }
+                ans+=j-i+1;
+                j++;
+            }
+            return ans;
+        };
+        return p(k)-p(k-1);
+        //return fun(nums,k,n)-fun(nums,k-1,n);
     }
 };
