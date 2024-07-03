67e55438a8c5b4b29a772b913a46d2cb7e416ca4
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-07-03 23:01:19 +0530

    Time: 75 ms (24.45%), Space: 38.8 MB (15.52%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -4,10 +4,11 @@ public:
         int n=nums.size();
         if(n<=4){return 0;}
         sort(nums.begin(),nums.end());
-        int ans=INT_MAX;
-        for(int i=0;i<4;i++){
-            ans=min(ans,nums[n-1 -3+i] -nums[i]);
-        }
-        return ans;
+        // int ans=INT_MAX;
+        // for(int i=0;i<4;i++){
+        //     ans=min(ans,nums[n-1 -3+i] -nums[i]);
+        // }
+        // return ans;
+        return min({(nums[n-4]-nums[0]),(nums[n-3]-nums[1]),(nums[n-2]-nums[2]),(nums[n-1]-nums[3]),});
     }
 };
