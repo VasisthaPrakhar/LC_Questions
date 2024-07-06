e5c61f602d72fda4d1d3ad734e532ed9588ed48d
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-07-06 22:23:40 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (71.18%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int passThePillow(int n, int t) {
-        if((t/(n-1))%2){
+        if(t/(n-1)%2){
             return n-(t%(n-1));
         }else{
-            return (t%(n-1))+1;
+            return t%(n-1)+1;
         }
     }
 };
