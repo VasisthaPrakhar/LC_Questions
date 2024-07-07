9487f3a64fae46e994d1978af00188648b948053
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-07-07 19:08:47 +0530

    Time: 2 ms (53.84%), Space: 7.1 MB (30.43%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numWaterBottles(int n, int e) {
+        int s=0,r=0;
+        while(n>=e){
+            int q=n/e;
+            r=n%e;
+            s+=(e*q);
+            n=q+r;
+        }
+        return s+n;
+    }
+};
