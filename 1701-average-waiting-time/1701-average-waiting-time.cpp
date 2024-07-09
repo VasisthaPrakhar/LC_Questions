3499250820418061b023a14001a2110bcdffaad6
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-07-09 22:53:43 +0530

    Time: 145 ms (72.06%), Space: 76.6 MB (35.00%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& c) {
+        double n=c.size();
+        double s=-1,s1=0;
+        for(int i=0;i<n;i++){
+            if(s==-1){
+                s=c[i][0]+c[i][1];
+                s1=s-c[i][0];
+            }else{
+                //cout<<s<<endl;
+                if(s>c[i][0]){
+                    s+=c[i][1];
+                }else{
+                    s=c[i][0]+c[i][1];
+                }
+                s1+=s-c[i][0];
+            }
+            //cout<<s<<" "<<s1<<endl;
+        }
+        return s1/n;
+    }
+};
