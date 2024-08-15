0bd06cd519ef0c666c72f72e4178c211cb8a628a
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-08-15 19:00:17 +0530

    Time: 68 ms (74.03%), Space: 86.1 MB (9.36%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int a[3]={0,0,0};
+        for(auto &x:bills){
+            if(x==5){
+                a[0]++;
+            }else if(x==10){
+                if(a[0]<1){
+                    return false;
+                }
+                a[0]--;
+                a[1]++;
+            }else{
+                if(a[1]>0){
+                    if(a[0]<1){
+                        return false;
+                    }
+                    a[0]--,a[1]--;
+                }else{
+                    if(a[0]<3){
+                        return false;
+                    }
+                    a[0]-=3;
+                }
+                a[2]++;
+            }
+        }
+        return true;
+    }
+};
