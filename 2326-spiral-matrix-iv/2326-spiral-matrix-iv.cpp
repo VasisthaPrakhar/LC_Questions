09a373e365883d7531ab4e31dc057454bb2ec4e3
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-09-09 21:22:19 +0530

    Time: 196 ms (5.11%), Space: 131.8 MB (5.62%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -13,7 +13,7 @@ public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
         vector<vector<int>>ans(m,vector<int>(n,-1));
         int i=0,j=0,f=0;
-        unordered_map<int,pair<int,int>>mp;
+        unordered_map<int,vector<int>>mp;
         mp[0]={0,1};
         mp[1]={1,0};
         mp[2]={0,-1};
@@ -21,12 +21,12 @@ public:
         while(head){
             //cout<<i<<" "<<j<<endl;
             ans[i][j]=head->val;
-            int nr=i+mp[f].first;
-            int nc=j+mp[f].second;
+            int nr=i+mp[f][0];
+            int nc=j+mp[f][1];
             if(nr<0 || nr>=m || nc<0 || nc>=n || ans[nr][nc]!=-1){
                 f=(f+1)%4;
-                i=i+mp[f].first;
-                j=j+mp[f].second;
+                i=i+mp[f][0];
+                j=j+mp[f][1];
             }else{
                 i=nr,j=nc;
             }
