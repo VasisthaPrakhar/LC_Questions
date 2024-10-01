6cb75508478226574fdb55ee03e9c004146bd6e8
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-10-01 21:01:45 +0530

    Time: 138 ms (31.35%), Space: 80 MB (11.90%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         int n=arr.size();
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            mp[(arr[i]%k+k)%k]++;
+            if(arr[i]<0){
+                arr[i]=arr[i]%k;
+            }
+            mp[(arr[i]+k)%k]++;
         }
         for(int i=0;i<k;i++){
             if(i==0){
