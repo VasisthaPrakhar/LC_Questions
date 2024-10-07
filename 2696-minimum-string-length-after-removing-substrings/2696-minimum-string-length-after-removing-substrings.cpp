4f5efb2edeeadb0a52e7a76648373a7aa48bd1f1
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-10-07 21:31:39 +0530

    Time: 8 ms (42.58%), Space: 11.5 MB (72.37%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minLength(string s) {
+        int n=s.length();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(!st.empty() && ((st.top()=='A' && s[i]=='B') || (st.top()=='C' && s[i]=='D'))){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        return st.size();
+    }
+};
