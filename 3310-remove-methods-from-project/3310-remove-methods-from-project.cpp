d3979c14600a58ada41873d82a049efa9b77f448
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-10-07 22:40:04 +0530

    Time: 1486 ms (8.33%), Space: 415.4 MB (8.33%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& in) {
+        map<int,set<int>>adj;
+        for(auto x:in){
+            adj[x[0]].insert(x[1]);
+        }
+        queue<int>q;
+        q.push(k);
+        vector<int>vis(n,0);
+        unordered_set<int>s;
+        vis[k]=1;
+        s.insert(k);
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            for(auto x:adj[node]){
+                if(!vis[x]){
+                    q.push(x);
+                    s.insert(x);
+                    vis[x]=1;
+                }
+            }
+        }
+        vector<int>ans;
+        int flag=0;
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                int f=0;
+                for(auto x:adj[i]){
+                    if(s.find(x)!=s.end()){
+                        f=1;
+                        flag=1;
+                        break;
+                    }
+                }
+                if(f==0){
+                    ans.push_back(i);
+                }
+            }
+        }
+        vector<int>res;
+        for(int i=0;i<n;i++){
+            res.push_back(i);
+        }
+        return flag==1?res:ans;
+    }
+};
