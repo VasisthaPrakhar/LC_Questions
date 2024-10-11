52497c85777e7507ffe4f0b3e782037174304b04
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-10-12 00:15:04 +0530

    Time: 168 ms (32.65%), Space: 74 MB (22.68%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -4,11 +4,12 @@ public:
         int n=t.size();
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         set<int>s;
-        set<pair<int,int>>v;
+        vector<pair<int,int>>v;
         for(int i=0;i<n;i++){
-            v.insert({t[i][0],i});
+            v.push_back({t[i][0],i});
             s.insert(i);
         }
+        sort(v.begin(),v.end());
         for(auto x:v){
             while(!pq.empty() && pq.top().first<=x.first){
                 s.insert(pq.top().second);
