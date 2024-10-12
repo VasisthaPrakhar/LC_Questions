e7f2ee1238a89cd3d16f297c12ecb6e7bc5ceafd
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-10-13 00:50:44 +0530

    Time: 2199 ms (9.09%), Space: 66.3 MB (13.64%) - LeetHub

--- 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
+++ 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int dp[3001][3001];
-    
-    int fun(int idx,int i,int j,vector<int>&arr,string &s,string &t,int n,int m,int sz){
+    int fun(int idx,int j,vector<int>&arr,string &s,string &t,int n,int m,int sz){
         if(j>=m){return sz-idx;}
+        int i=0;
+        if(idx>0){
+            i=arr[idx-1]+1;
+        }
         if(i>=n){return INT_MIN;}
         while(i<n && j<m){
             if(idx<sz &&  i==arr[idx]){
@@ -21,18 +24,23 @@ public:
             else 
                 return INT_MIN;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(dp[idx][j]!=-1){
+            return dp[idx][j];
         }
-        int res=INT_MIN,f=0;
-        res=max(res,1+fun(idx+1,i+1,j,arr,s,t,n,m,sz));
-        res=max(res,fun(idx+1,i,j,arr,s,t,n,m,sz));
-        return dp[i][j]=res;        
+        int res=INT_MIN;
+        if(s[i]==t[j]){
+            res=max(res,fun(idx+1,j+1,arr,s,t,n,m,sz));
+            res=max(res,1+fun(idx+1,j,arr,s,t,n,m,sz));
+        }else{
+            res=max(res,1+fun(idx+1,j,arr,s,t,n,m,sz));
+        }
+        
+        return dp[idx][j]=res;        
     }
     int maxRemovals(string s, string t, vector<int>& arr) {
         if(s==t){return 0;}
         int n=s.length(),m=t.length(),sz=arr.size();
         memset(dp,-1,sizeof(dp));
-        return fun(0,0,0,arr,s,t,n,m,sz);
+        return fun(0,0,arr,s,t,n,m,sz);
     }
 };
