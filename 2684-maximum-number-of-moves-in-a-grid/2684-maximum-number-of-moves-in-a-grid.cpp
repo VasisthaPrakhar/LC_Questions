89f4c26b4c30f26f0ca4fa11844740691c7113f3
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-10-29 21:19:16 +0530

    Time: 42 ms (32.61%), Space: 74.2 MB (68.93%) - LeetHub

---
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxMoves(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size(),ans=0;
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
+        for(int j=m-2;j>=0;j--){
+            int res=0;
+            for(int i=0;i<n;i++){
+                if(i>0 && grid[i][j]<grid[i-1][j+1]){
+                    dp[i][j]=max(dp[i][j],1+dp[i-1][j+1]);
+                }
+                if(i<n-1 && grid[i][j]<grid[i+1][j+1]){
+                    dp[i][j]=max(dp[i][j],1+dp[i+1][j+1]);
+                }
+                if(grid[i][j]<grid[i][j+1]){
+                    dp[i][j]= max(dp[i][j],1+dp[i][j+1]);
+                }
+                res=max(dp[i][j],res);
+            }
+            ans=res;
+        }
+        return ans;
+    }
+};
