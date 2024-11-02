0be946e5579ecc3c2a38645adc28831a5ce7bf66
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-11-02 23:08:29 +0530

    Time: 32 ms (58.21%), Space: 20.6 MB (40.58%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     typedef long long int ll;
-    vector<vector<ll>>dp;
-    ll fun(int i,int j,vector<int>&a,vector<int>&b){
-        if(i<0){
-            return 0;
-        }
-        if(j<0){
-            return (ll)1e12;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        ll res=LLONG_MAX;
-        res=min(res,abs(a[i]-b[j])+fun(i-1,j-1,a,b));
-        res=min(res,fun(i,j-1,a,b));
-        return dp[i][j]=res;
-    }
+    // vector<vector<ll>>dp;
+    // ll fun(int i,int j,vector<int>&a,vector<int>&b){
+    //     if(i<0){
+    //         return 0;
+    //     }
+    //     if(j<0){
+    //         return (ll)1e12;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     ll res=LLONG_MAX;
+    //     res=min(res,abs(a[i]-b[j])+fun(i-1,j-1,a,b));
+    //     res=min(res,fun(i,j-1,a,b));
+    //     return dp[i][j]=res;
+    // }
     long long minimumTotalDistance(vector<int>& r, vector<vector<int>>& f) {
         sort(r.begin(),r.end());
         sort(f.begin(),f.end());
@@ -28,7 +28,10 @@ public:
             }
         }
         int n=r.size(),m=pos.size();
-        dp.resize(n+1,vector<ll>(m+1,0));
+        // dp.resize(n,vector<ll>(m,-1));
+        // return fun(n-1,m-1,r,pos);
+        ll dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
         for(int i=1;i<=n;i++){
             dp[i][0]=1e12;
         }
