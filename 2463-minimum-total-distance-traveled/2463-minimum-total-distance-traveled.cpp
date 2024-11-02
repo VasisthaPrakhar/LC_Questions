e565892ac8071359167f66a274a68b16117dc90a
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-11-02 23:01:55 +0530

    Time: 127 ms (29.53%), Space: 68.2 MB (16.09%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     typedef long long int ll;
     vector<vector<ll>>dp;
-    ll fun(int i,int j,int n,int m,vector<int>&a,vector<int>&b){
-        if(i>=n){
+    ll fun(int i,int j,vector<int>&a,vector<int>&b){
+        if(i<0){
             return 0;
         }
-        if(j>=m){
+        if(j<0){
             return (ll)1e12;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         ll res=LLONG_MAX;
-        res=min(res,abs(a[i]-b[j])+fun(i+1,j+1,n,m,a,b));
-        res=min(res,fun(i,j+1,n,m,a,b));
+        res=min(res,abs(a[i]-b[j])+fun(i-1,j-1,a,b));
+        res=min(res,fun(i,j-1,a,b));
         return dp[i][j]=res;
     }
     long long minimumTotalDistance(vector<int>& r, vector<vector<int>>& f) {
@@ -29,6 +29,6 @@ public:
         }
         int n=r.size(),m=pos.size();
         dp.resize(n,vector<ll>(m,-1));
-        return fun(0,0,n,m,r,pos);
+        return fun(n-1,m-1,r,pos);
     }
 };
