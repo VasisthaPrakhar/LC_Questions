9f631560f8be8049a718425cabc286d5c1d43fc6
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-11-03 22:15:29 +0530

    Time: 1 ms (36.36%), Space: 8.1 MB (36.36%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isBalanced(string s) {
+        int n=s.length(),a=0,b=0;
+        for(int i=0;i<n;i++){
+            if(i%2){
+                b+=s[i]-'0';
+            }else{
+                a+=s[i]-'0';
+            }
+        }
+        return a==b;
+    }
+};
