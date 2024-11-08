c4ba64debae2f4888cf0ca3ce2f30015cd53c8dd
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-11-09 00:17:12 +0530

    Time: 34 ms (8.57%), Space: 99.8 MB (27.33%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -3,19 +3,20 @@ public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
         int n=nums.size(),a=0;
         vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     a^=nums[i];
+        // }
         for(int i=0;i<n;i++){
             a^=nums[i];
-        }
-        for(int i=n-1;i>=0;i--){
-            int k=0;;
+            int k=0;
             for(int j=0;j<maximumBit;j++){
                 if(!(a & (1<<j))){
                     k=k|(1<<j);
                 }
             }
             ans.push_back(k);
-            a^=nums[i];
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
