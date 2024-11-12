ed2524235c75c0afc342094f1a9879c80e30147c
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-11-12 21:51:20 +0530

    Time: 96 ms (11.48%), Space: 110.2 MB (10.81%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -1,36 +1,22 @@
-static const auto Initialize = []{
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& it, vector<int>& q) {
-        int n=it.size();
-        int m=q.size();
+        int n=it.size(),ma=0;
         sort(it.begin(),it.end());
-        map<int,int>mp;
-        mp[0]=0;
-        mp[it[0][0]]=it[0][1];
-        int ma=0;
-        for(int i=1;i<n;i++){
-            it[i][1]=it[i][1]>it[i-1][1]?it[i][1]:it[i-1][1];
-            mp[it[i][0]]=it[i][1];
-        }
-        vector<int>ans;
-        for(int i=0;i<m;i++){
-            if(!mp.count(q[i])){
-                mp[q[i]]=0;
-            }
-        }
-        for(auto it=mp.begin();it!=mp.end();it++){
-            auto it1=it;
-            it1--;
-            if(it->second==0){
-                it->second=it1->second;
-            }
+        unordered_map<int,int>mp;
+        vector<int>a,ans;
+        for(int i=0;i<n;i++){
+            ma=max(ma,it[i][1]);
+            a.push_back(it[i][0]);
+            mp[i]=ma;
         }
         for(auto x:q){
-            ans.push_back(mp[x]);
+            auto f=upper_bound(a.begin(),a.end(),x)-a.begin();
+            if(f==0){
+                ans.push_back(0);
+            }else{
+                ans.push_back(mp[f-1]);
+            }
         }
         return ans;
     }
