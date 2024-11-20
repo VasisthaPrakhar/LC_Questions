d107151b9bd3f6271147624e92b85d4153fae663
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-11-20 19:53:18 +0530

    Time: 1209 ms (5.37%), Space: 15.3 MB (11.18%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
-    bool check(vector<int>&v,vector<int>&v1,int k){
-        int a=v[0]+v1[0],b=v[1]+v1[1],c=v[2]+v1[2];
-        if(a<k || b<k || c<k){
-            return false;
-        }
-        return true;
+    bool check(unordered_map<char,int>&m,int k){
+        return (m['a']>=k) && (m['b']>=k) && (m['c']>=k) && m['z']>0;
     }
-    int takeCharacters(string s, int k) {
-        unordered_map<int,vector<int>>ml,mr;
-        int n=s.length();
-        if(k==0){
-            return 0;
+    int takeCharacters(string &s, int k) {
+        int sz=s.length();
+        if(k==0){return 0;}
+        if(sz<3){return -1;}
+        string t=s+"z"+s;
+        int j=0,i=0,n=t.length(),ans=INT_MAX;
+        unordered_map<char,int>m;
+        while(j<n){
+            m[t[j]]++;
+            while(i<=j && check(m,k)){
+                ans=min(ans,j-i+1);
+                cout<<ans<<endl;
+                m[t[i]]--;
+                i++;
+            }
+            j++;
         }
-        int ans=n;
-        int a=0,b=0,c=0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='a'){
-                a++;
-            }else if(s[i]=='b'){
-                b++;
-            }else{
-                c++;
-            }
-            if(a>=k && b>=k && c>=k){
-                ans=min(ans,i+1);
-            }
-            ml[i]={a,b,c};
-        }
-        if(a<k || b<k || c<k){
-            return -1;
-        }
-        a=0,b=0,c=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='a'){
-                a++;
-            }else if(s[i]=='b'){
-                b++;
-            }else{
-                c++;
-            }
-            if(a>=k && b>=k && c>=k){
-                ans=min(ans,n-i);
-            }
-            mr[i]={a,b,c};
-        }
-        int j=0;
-        for(int i=0;i<n;i++){
-            int f=0;
-            while(j<n && check(ml[i],mr[j],k)){
-                j++;
-                f=1;
-            }
-            if(f){
-                j--;
-            }
-            ans=min(ans,n-(j-i-1));
-        }
-        return ans;
+        return (ans==INT_MAX || (ans-1)>sz)?-1:ans-1;
     }
 };
