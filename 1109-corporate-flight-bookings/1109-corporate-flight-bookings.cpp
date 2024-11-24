a81a724c4b82a221a911da7195c7ae14aa5317ab
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-11-25 00:22:14 +0530

    Time: 2 ms (69.26%), Space: 71.6 MB (64.91%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& b, int n) {
         vector<int>ans(n+1,0);
-        for(auto x:b){
+        for(auto &x:b){
             ans[x[0]-1]+=x[2];
             ans[x[1]]-=x[2];
         }
