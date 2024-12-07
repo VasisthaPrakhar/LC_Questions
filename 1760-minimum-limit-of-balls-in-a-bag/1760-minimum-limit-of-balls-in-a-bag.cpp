c509b718c3d605e368627cc201edd5e83a38e1e8
Prakhar Vasistha <work.prakharvasistha@gmail.com>
2024-12-08 01:56:12 +0530

    Time: 39 ms (41.23%), Space: 59.9 MB (15.30%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -11,16 +11,15 @@ public:
     }
     int minimumSize(vector<int>& nums, int mo) {
         int n=nums.size();
-        ll beg=1,end=*max_element(nums.begin(),nums.end()),mid,ans=end;
+        ll beg=1,end=*max_element(nums.begin(),nums.end()),mid;
         while(beg<end){
             mid=(beg+end)/2;
             if(fun(nums,mid,mo)){
-                ans=min(ans,mid);
                 end=mid;
             }else{
                 beg=mid+1;
             }
         }
-        return ans;
+        return end;
     }
 };
